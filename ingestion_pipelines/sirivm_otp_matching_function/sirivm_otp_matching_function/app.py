9d658ce2345266d0ff395a922709ba16a1ec2600
Levina <levina.chan@kpmg.co.uk>
2024-10-09 16:20:45 +0100

    rearrange order of of code in matching lambda

--- ingestion_pipelines/sirivm_otp_matching_function/sirivm_otp_matching_function/app.py
+++ ingestion_pipelines/sirivm_otp_matching_function/sirivm_otp_matching_function/app.py
@@ -42,18 +42,16 @@ def lambda_handler(event: dict[str, Any], _: LambdaContext) -> None:
             backfill_record_handler(rec, _cache["shards"])
             continue
 
-        if "main_timetable" not in _cache:
-            _cache["main_timetable"] = s3_client.download_main_timetable()
-
-        if (
-            rec.message_attributes["key"].string_value == "timetable"
-            and "main_timetable" in _cache
-        ):
+        if rec.message_attributes["key"].string_value == "timetable":
             # Invalidate timetable cache, it will be when next needed
             # We probably want to switch to a time based TTL on the cache
-            del _cache["main_timetable"]
-        else:
-            live_record_handler(rec, _cache["shards"], _cache["main_timetable"])
+            if "main_timetable" in _cache:
+                del _cache["main_timetable"]
+
+        if "main_timetable" not in _cache:
+            _cache["main_timetable"] = s3_client.download_main_timetable()
+        
+        live_record_handler(rec, _cache["shards"], _cache["main_timetable"])
 
 
 def backfill_record_handler(rec: SQSRecord, shards: dict[str, Any]) -> None:
