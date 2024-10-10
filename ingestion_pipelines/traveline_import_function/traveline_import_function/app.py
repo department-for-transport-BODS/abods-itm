e1a42fd777dbfae4a36a533b4e55e0ce53da3375
Laurence Jones <laurence.jones@doza.consulting>
2024-10-10 16:12:22 +0100

    refactored traveline import function

--- ingestion_pipelines/traveline_import_function/traveline_import_function/app.py
+++ ingestion_pipelines/traveline_import_function/traveline_import_function/app.py
@@ -1,91 +1,56 @@
-import logging
-from os import environ
-
-import boto3
 import petl
-import psycopg2
+from aws_lambda_powertools import Logger
+from psycopg2.extras import execute_values
 
-db_host = environ.get("POSTGRES_HOST")
-db_port = environ.get("POSTGRES_PORT")
-db_user = environ.get("POSTGRES_USER")
-db_database = environ.get("POSTGRES_DB")
+from .shared.db import setup_db
 
-logger = logging.getLogger("sirivm")
-logging.getLogger().setLevel("INFO")
+TRAVELINE_NOC_URL = "https://www.travelinedata.org.uk/wp-content/themes/desktop/qeight_download.php?allGroupsD=-1&allRegionD=-1&allModeD=-1&allCessationD=-1&searchTextD=&maxPage=231&selectPageId=1&downloadType=CSV&submit=Download"
 
-
-def get_rds_token():
-    session = boto3.Session()
-    client = session.client("rds")
-    try:
-        region = "eu-west-2"
-        token = client.generate_db_auth_token(
-            DBHostname=db_host,
-            Port=db_port,
-            Region=region,
-            DBUsername=db_user,
-        )
-    except Exception as e:
-        logging.exception("could not get token", e)
-        raise e
-
-    return token
+logger = Logger()
+conn = setup_db()
 
 
-def lambda_handler(event, context):
-    try:
-        url = "https://www.travelinedata.org.uk/wp-content/themes/desktop/qeight_download.php?allGroupsD=-1&allRegionD=-1&allModeD=-1&allCessationD=-1&searchTextD=&maxPage=231&selectPageId=1&downloadType=CSV&submit=Download"
-        logging.info(f"Getting data from url {url}")
-        noc_table = petl.fromcsv(url).distinct("NOCCODE")
+def lambda_handler(_event: dict, _context: dict) -> None:
+    logger.info("Retrieving data from Traveline")
 
-        logging.info("Converting data to tuples")
-        rows = tuple(
-            (row["NOCCODE"], row["OperatorPublicName"], row["Licence"], row["Mode"])
-            for row in noc_table.dicts()
+    noc_table = petl.fromcsv(TRAVELINE_NOC_URL).distinct("NOCCODE")
+
+    logger.info("Converting data to tuples")
+
+    rows = tuple(
+        (row["NOCCODE"], row["OperatorPublicName"], row["Licence"], row["Mode"])
+        for row in noc_table.dicts()
+    )
+
+    with conn.cursor() as cur:
+        logger.info("Attempting to create table if not exists")
+
+        cur.execute(
+            """
+            CREATE TABLE IF NOT EXISTS traveline_operators (
+                noc_code varchar NOT NULL,
+                "name" varchar NULL,
+                licence varchar NULL,
+                "mode" varchar NULL,
+                CONSTRAINT travelinedata_pk PRIMARY KEY (noc_code)
+            );
+            """,
         )
 
-        logging.info(f"Connecting to db {db_host}")
-        conn = psycopg2.connect(
-            host=db_host,
-            port=db_port,
-            database=db_database,
-            user=db_user,
-            password=get_rds_token(),
-            sslmode="require",
+        logger.info("Inserting NOC data")
+
+        execute_values(
+            cur,
+            """
+            INSERT INTO traveline_operators (
+                noc_code,
+                name,
+                licence,
+                mode
+            )
+            VALUES %s
+            ON CONFLICT (noc_code)
+                DO UPDATE SET name = EXCLUDED.name
+            """,
+            rows,
         )
-        with conn.cursor() as cursor:
-            logging.info("Attempting to create table if not exists")
-            cursor.execute(
-                """
-                    CREATE TABLE IF NOT EXISTS traveline_operators (
-                        noc_code varchar NOT NULL,
-                        "name" varchar NULL,
-                        licence varchar NULL,
-                        "mode" varchar NULL,
-                        CONSTRAINT travelinedata_pk PRIMARY KEY (noc_code)
-                    );
-                """,
-            )
-            logging.info("Mogrifying input data")
-            args_str = ",".join(
-                cursor.mogrify("(%s,%s, %s, %s)", x).decode("utf-8") for x in rows
-            )
-
-            logging.info("Inserting input data")
-            cursor.execute(
-                f"""
-                    INSERT INTO traveline_operators (
-                        noc_code,
-                        name,
-                        licence,
-                        mode
-                    )
-                    VALUES {args_str}
-                    ON CONFLICT (noc_code)
-                    do update set name = EXCLUDED.name""",
-            )
-
-            logging.info("Committing data")
-            conn.commit()
-    except Exception as e:
-        print("Couldn't write to database: ", e)
