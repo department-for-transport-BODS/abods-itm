7d4d65bf5e47425de38a61cf7fd3b8a213ff83ee
Gareth Somerville <gareth.somerville@doza.consulting>
2024-10-10 14:13:17 +0100

    Improved logging in otp matching handler

--- ingestion_pipelines/sirivm_otp_matching_function/sirivm_otp_matching_function/app.py
+++ ingestion_pipelines/sirivm_otp_matching_function/sirivm_otp_matching_function/app.py
@@ -30,85 +30,113 @@ def read_timetable(timetable_name: str) -> dict[str, RouteDetails]:
 _cache = {}
 
 
-@logger.inject_lambda_context(log_event=True, clear_state=True)
+@logger.inject_lambda_context(log_event=True)
 @timer(logger)
 def lambda_handler(event: dict[str, Any], _: LambdaContext) -> None:
     sqs_event = SQSEvent(event)
-    if "shards" not in _cache:
-        _cache["shards"] = s3_client.get_shards()
+
+    base_keys = set(logger.get_current_keys().keys())
 
     for rec in sqs_event.records:
+        # Only needed if an event can have multiple records
+        current_keys = set(logger.get_current_keys().keys())
+        logger.remove_keys(current_keys - base_keys)
+
+        logger.append_keys(message_attributes=rec.message_attributes)
+
+        if "shards" not in _cache:
+            logger.info("Getting shards data")
+            _cache["shards"] = s3_client.get_shards()
+
         if "Historic" in rec.message_attributes:
-            backfill_record_handler(rec, _cache["shards"])
+            historic_record_handler(rec, _cache["shards"])
             continue
 
         if rec.message_attributes["key"].string_value == "timetable":
+            if "main_timetable" not in _cache:
+                logger.info(
+                    "Request to refresh timetable received, but not yet in cache",
+                )
+                continue
+
             # Invalidate timetable cache, it will be when next needed
             # We probably want to switch to a time based TTL on the cache
-            if "main_timetable" in _cache:
-                del _cache["main_timetable"]
+            logger.info("Clearing main timetable")
+            del _cache["main_timetable"]
             continue
 
         if "main_timetable" not in _cache:
+            logger.info("Fetching main timetable")
             _cache["main_timetable"] = s3_client.download_main_timetable()
 
         live_record_handler(rec, _cache["shards"], _cache["main_timetable"])
 
 
-def backfill_record_handler(rec: SQSRecord, shards: dict[str, Any]) -> None:
-    """Fetch the historic avl record and timetable to do historic otp matching"""
+def historic_record_handler(rec: SQSRecord, shards: dict[str, Any]) -> None:
+    """Fetch the historic AVL records and timetable to do historic OTP matching"""
+    logger.append_keys(historic=True)
+    logger.info("Processing historic record")
+
+    fname = rec.message_attributes["key"].string_value
+    shard_identifier = rec.message_attributes["shard"].string_value
+
     try:
-        fname = rec.message_attributes["key"].string_value
-        shard_no = rec.message_attributes["shard"].string_value
-        logger.info(f"OTP data being processed for file {fname}")
-        # find a timetable for matching
         avl_time = fname[fname.index("avl_") + 4 : -3]
         avl_datetime = parse(avl_time)
-        avl_year = avl_datetime.year
-        avl_month = str(avl_datetime.month).zfill(2)
-        avl_day = str(avl_datetime.day).zfill(2)
-        avl_date_str = f"{avl_year}-{avl_month}-{avl_day}"
-        avl_hour = str(avl_datetime.hour).zfill(2)
-        half_hour = 30
-        avl_minute = "00" if avl_datetime.minute < half_hour else "30"
-        timetable_dir = f"timetable_shreds/YYYY={avl_year}/MM={avl_month}/DD={avl_day}/"
-        timetable_name = (
-            f"timetable_{avl_year}{avl_month}{avl_day}_{avl_hour}_{avl_minute}.json"
-        )
-        timetable = read_timetable(timetable_dir + timetable_name)
-        # fetch timetable s3
+        logger.append_keys(avl_time=avl_time, avl_datetime=avl_datetime)
+
+        logger.info("Fetching stop history")
         shard_stop_history = s3_client.get_stop_history(
             avl_datetime,
-            shard_no,
+            shard_identifier,
             int(avl_time),
         )
 
         # for recovery, only process avl file that is greater than last process avl file
         if int(avl_time) < int(shard_stop_history["control_info"]["last_avl"]):
-            logger.info(f"{avl_time} has been processed, skipping.")
+            logger.info("Record has already been processed, skipping")
             return
 
-        # clean stop history
+        logger.info("Cleaning stop history")
         clean_shard_stop_history = clean_stop_history(shard_stop_history, avl_datetime)
 
-        # fetch avl data
-        avl_dict = s3_client.get_avl_data(fname)
+        avl_year = avl_datetime.year
+        avl_month = str(avl_datetime.month).zfill(2)
+        avl_day = str(avl_datetime.day).zfill(2)
+        avl_hour = str(avl_datetime.hour).zfill(2)
+        half_hour = 30
+        avl_minute = "00" if avl_datetime.minute < half_hour else "30"
+        timetable_key = f"timetable_shreds/YYYY={avl_year}/MM={avl_month}/DD={avl_day}/timetable_{avl_year}{avl_month}{avl_day}_{avl_hour}_{avl_minute}.json"
+        logger.append_keys(timetable_key=timetable_key)
+        logger.info("Fetching historic timetable")
+        timetable = read_timetable(timetable_key)
 
-        logger.info("Run matching")
+        logger.info("Fetching AVL data")
+        avl_list = s3_client.get_avl_data(fname)
+
+        logger.info("Got data, calculating matches")
         to_set, to_remove, stop_history = positions_timetable_lookup(
             timetable,
             shards,
-            shard_no,
-            avl_dict,
+            shard_identifier,
+            avl_list,
             None,
             clean_shard_stop_history,
         )
-        db_client.historic_update_success(to_set, to_remove, avl_date_str)
-        logger.info(f"{fname} historic matching successful")
-        s3_client.export_stop_history(stop_history, avl_datetime, shard_no)
-        logger.info(f"OTP data updated for file {fname}")
+
+        logger.info("Updating database with successful results")
+        db_client.historic_update_success(
+            to_set,
+            to_remove,
+            f"{avl_year}-{avl_month}-{avl_day}",
+        )
+
+        logger.info("Saving stop history")
+        s3_client.export_stop_history(stop_history, avl_datetime, shard_identifier)
+
+        logger.info("Processing complete")
     except Exception:
-        logger.exception("An error occurred when processing backfill record")
+        logger.exception("An error occurred when processing historic record")
 
 
 def live_record_handler(
@@ -116,37 +144,51 @@ def live_record_handler(
     shards: dict[str, Any],
     timetable: dict[str, Any],
 ) -> None:
-    current_date = datetime.today()  # noqa: DTZ002 - Stop using today() later
+    """Fetch the live AVL records and timetable to do live OTP matching"""
+    logger.append_keys(historic=False)
+    logger.info("Processing live record")
+
     fname = rec.message_attributes["key"].string_value
     batch_id = int(rec.message_attributes["batch_id"].string_value)
-    shard_no = rec.message_attributes["shard"].string_value
+    shard_identifier = rec.message_attributes["shard"].string_value
+
     try:
-        logger.info(f"OTP data being processed for file {fname}")
         # Check if avl file coming in in order
         avl_time_val = int(fname[-17:-3])
         avl_datetime = parse(str(avl_time_val))
-        avl_dict = s3_client.get_avl_data(fname)
-        # read stop history of the shard
+        logger.append_keys(avl_time=avl_time_val, avl_datetime=avl_datetime)
+
+        logger.info("Fetching stop history")
+        current_date = datetime.today()  # noqa: DTZ002 - Stop using today() later
         shard_stop_history = s3_client.get_stop_history(
             current_date,
-            shard_no,
+            shard_identifier,
             avl_time_val,
         )
-        # clean stop history
+
+        logger.info("Cleaning stop history")
         clean_shard_stop_history = clean_stop_history(shard_stop_history, avl_datetime)
 
+        logger.info("Fetching AVL data")
+        avl_list = s3_client.get_avl_data(fname)
+
+        logger.info("Got data, calculating matches")
         to_set, to_remove, stop_history = positions_timetable_lookup(
             timetable,
             shards,
-            shard_no,
-            avl_dict,
+            shard_identifier,
+            avl_list,
             batch_id,
             clean_shard_stop_history,
         )
-        db_client.live_update_success(batch_id, to_set, to_remove)
-        s3_client.export_stop_history(stop_history, current_date, shard_no)
-        logger.info(f"OTP data updated for file {fname}")
 
+        logger.info("Updating database")
+        db_client.live_update_success(batch_id, to_set, to_remove)
+
+        logger.info("Updating S3")
+        s3_client.export_stop_history(stop_history, current_date, shard_identifier)
+
+        logger.info("Processing complete")
     except Exception:
         logger.exception("An error occurred when processing the record")
         db_client.batch_failed(batch_id)
