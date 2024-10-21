5579e96031e2aa9d5866c7db5471b61ca60b0762
Gareth Somerville <gareth.somerville@doza.consulting>
2024-10-21 18:10:04 +0100

    returning clause on updates

--- ingestion_pipelines/sirivm_otp_matching_function/sirivm_otp_matching_function/sql/remove_historic_matching.sql
+++ ingestion_pipelines/sirivm_otp_matching_function/sirivm_otp_matching_function/sql/remove_historic_matching.sql
@@ -1,8 +1,10 @@
-update  public."Timetable" u
-set
-    time_difference = null,
-    actual_departure_time = null,
-    otp_state = null,
+UPDATE public."Timetable" u
+SET time_difference = NULL,
+    actual_departure_time = NULL,
+    otp_state = NULL,
     load_time_stamp = now()::timestamp(0)
-from (values %s) as t(timetable_id, group_id, journey_date)
-where u.timetable_id = t.timetable_id::int and date_of_journey = t.journey_date::date and u.group_id = t.group_id;
+FROM (VALUES %s) AS t(timetable_id, group_id, journey_date)
+WHERE u.timetable_id = t.timetable_id::int
+  AND date_of_journey = t.journey_date::date
+  AND u.group_id = t.group_id
+RETURNING load_time_stamp;
--- ingestion_pipelines/sirivm_otp_matching_function/sirivm_otp_matching_function/sql/set_historic_matching.sql
+++ ingestion_pipelines/sirivm_otp_matching_function/sirivm_otp_matching_function/sql/set_historic_matching.sql
@@ -1,9 +1,19 @@
 -- Recalculating time difference when it's less than zero to make sure it's calculated correctly
-update  public."Timetable" u
-set
-    time_difference = case when t.time_difference::int < 0 then extract(epoch from (t.last_time_in_zone_utc::timestamp at TIME zone 'utc' - u.expected_departure_time::timestamp))::int else t.time_difference::int end,
-    actual_departure_time = t.last_time_in_zone_utc::timestamp at TIME zone 'utc',
+UPDATE public."Timetable" u
+SET time_difference = CASE
+                          WHEN t.time_difference::int < 0 THEN extract(epoch
+                                                                       FROM (t.last_time_in_zone_utc::timestamp AT TIME ZONE
+                                                                              'UTC' - u.expected_departure_time::timestamp))::int
+                          ELSE t.time_difference::int
+                      END,
+    actual_departure_time = t.last_time_in_zone_utc::timestamp AT TIME ZONE 'UTC',
     load_time_stamp = now()::timestamp(0)
-from (values %s) as t(time_difference, last_time_in_zone_str, timetable_id, group_id, batch_id, last_time_in_zone_utc, otp_state, is_final_stop, journey_date)
-where u.timetable_id = t.timetable_id::int and date_of_journey = t.journey_date::date and coalesce(extract (epoch from (t.last_time_in_zone_utc::timestamp at TIME zone 'utc' - u.expected_departure_time)),0) > -7200;
+FROM (VALUES %s) AS t(time_difference, last_time_in_zone_str, timetable_id, group_id, batch_id, last_time_in_zone_utc, otp_state, is_final_stop, journey_date)
+WHERE u.timetable_id = t.timetable_id::int
+  AND date_of_journey = t.journey_date::date
+  AND COALESCE(
+              EXTRACT(epoch FROM (t.last_time_in_zone_utc::timestamp AT TIME ZONE 'UTC' - u.expected_departure_time)),
+              0
+      ) > -7200
+RETURNING load_time_stamp;
                     
