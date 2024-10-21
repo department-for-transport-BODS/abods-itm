ca73eb0c882cf54c07200d5edb34f7ddbfd28e49
Levina <levina.chan@kpmg.co.uk>
2024-10-21 12:16:10 +0100

    udpate remove live matching sql and add logging

--- ingestion_pipelines/sirivm_otp_matching_function/sirivm_otp_matching_function/sql/remove_live_matching.sql
+++ ingestion_pipelines/sirivm_otp_matching_function/sirivm_otp_matching_function/sql/remove_live_matching.sql
@@ -5,5 +5,4 @@ set
     otp_state = null,
     load_time_stamp = now()::timestamp(0)
 from (values %s) as t(stop_ind, timetable_id, group_id)
-where u.timetable_id = t.timetable_id::int and date_of_journey = now()::date and u.group_id = t.group_id and u.stop_index = t.stop_ind::int;
-                
+where u.timetable_id = t.timetable_id::int and date_of_journey = now()::date and u.group_id = t.group_id;
