0ccba6a0fb5db2625ea1a08769725b5030c3c185
Gareth Somerville <gareth.somerville@doza.consulting>
2024-12-04 16:40:18 +0000

    Fix name of primary key to be deleted (#195)
    * Fix name of primary key to be deleted
    
    * bump version

--- scripts/db_connect.sh
+++ scripts/db_connect.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-set -e
-set -u
-set -o pipefail
+set -euo pipefail
 
-INSTANCE=$(aws ec2 describe-instances --region=eu-west-2 --filters Name=tag:Type,Values=Bastion Name=instance-state-name,Values=running | jq -r '.Reservations[0].Instances[0]')
-INSTANCE_ID=`echo ${INSTANCE} | jq -r '.InstanceId'`
-DATABASE_ENDPOINT=$(aws rds describe-db-clusters | jq -r '.DBClusters[0].Endpoint')
-AZ=`echo ${INSTANCE} | jq -r '.Placement.AvailabilityZone'`
-LOCAL_PORT=15432
+environment="${1:-sandbox}"
 REGION="eu-west-2"
+INSTANCE_ID=$(aws ec2 describe-instances --region=$REGION --filters Name=tag:Name,Values=abods-$environment-bastion Name=instance-state-name,Values=running | jq -r '.Reservations[0].Instances[0].InstanceId')
+DATABASE_ENDPOINT=$(aws rds describe-db-clusters | jq -r ".DBClusters[] | select(.DBClusterIdentifier | startswith(\"abods-$environment\")) | .Endpoint")
+LOCAL_PORT=15432
 
 echo "starting ssh database tunnel"
 echo "bastion instance: ${INSTANCE_ID}"
@@ -18,7 +15,7 @@ echo "local port: ${LOCAL_PORT}"
 echo "region: ${REGION}"
 
 aws ssm start-session \
-    --region $REGION \
-    --target $INSTANCE_ID \
+    --region "$REGION" \
+    --target "$INSTANCE_ID" \
     --document-name AWS-StartPortForwardingSessionToRemoteHost \
     --parameters host=$DATABASE_ENDPOINT,portNumber="5432",localPortNumber=$LOCAL_PORT
