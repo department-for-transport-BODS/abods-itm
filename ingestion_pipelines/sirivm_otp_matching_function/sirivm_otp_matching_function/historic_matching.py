6eec48352db412713426a32419fc9df8958ac40b
Gareth Somerville <gareth.somerville@doza.consulting>
2024-12-06 10:09:42 +0000

    Move historic matching dockerfile into where CI expects it

--- ingestion_pipelines/sirivm_otp_matching_function/sirivm_otp_matching_function/historic_matching.py
+++ ingestion_pipelines/sirivm_otp_matching_function/sirivm_otp_matching_function/historic_matching.py
@@ -1,3 +1,6 @@
+# This file is the entry point of an ECS task that performs a long-lived matching process.
+# It lives alongside the lambda code for ease of development
+
 import json
 import os
 import sys
@@ -197,6 +200,10 @@ if __name__ == "__main__":
     if "PROCESS_DATE" not in os.environ:
         logger.error("Environment variable PROCESS_DATE is missing.")
         sys.exit(1)
+    #temp
+    logger.info("startup successful")
+    sys.exit(0)
+
     process_date = os.environ["PROCESS_DATE"]
     process_date_parts = process_date.split("-")
     year = process_date_parts[0]
