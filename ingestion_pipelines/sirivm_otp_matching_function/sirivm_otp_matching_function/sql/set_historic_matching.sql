c5cc833bcefaa05c8604ca4a6e367374155f08a2
Levina <levina.chan@kpmg.co.uk>
2024-12-09 15:19:53 +0000

    fix coalesce datatype error

--- ingestion_pipelines/sirivm_otp_matching_function/sirivm_otp_matching_function/sql/set_historic_matching.sql
+++ ingestion_pipelines/sirivm_otp_matching_function/sirivm_otp_matching_function/sql/set_historic_matching.sql
@@ -2,7 +2,7 @@
 UPDATE public."Timetable" u
 SET time_difference = CASE
                           WHEN t.time_difference::int < 0 THEN
-                              EXTRACT(epoch FROM (COALESCE(t.last_time_in_zone_utc, t.timestamp_after_estimate)::timestamp AT TIME ZONE 'UTC' - u.expected_departure_time::timestamp))::int
+                              EXTRACT(epoch FROM (COALESCE(t.last_time_in_zone_utc, t.timestamp_after_estimate::timestamp)::timestamp AT TIME ZONE 'UTC' - u.expected_departure_time::timestamp))::int
                           ELSE t.time_difference::int
                       END,
     actual_departure_time = t.last_time_in_zone_utc::timestamp AT TIME ZONE 'UTC',
