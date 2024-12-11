eb9f2bb013d7d606cd6d68f422b4bf58bc17fa62
Gareth Somerville <gareth.somerville@doza.consulting>
2024-12-11 16:03:02 +0000

    Single database connection and log queue size

--- ingestion_pipelines/sirivm_otp_matching_function/sirivm_otp_matching_function/historic_matching.py
+++ ingestion_pipelines/sirivm_otp_matching_function/sirivm_otp_matching_function/historic_matching.py
@@ -9,6 +9,7 @@ from multiprocessing import Process, Queue
 import boto3
 import duckdb
 from aws_lambda_powertools import Logger
+from duckdb.duckdb import DuckDBPyConnection
 
 from .client_db import TimetableDBClient
 from .matcher.live_timetable_store import LiveTimetableStore
@@ -17,7 +18,6 @@ from .matcher.models import AVLRecord, StopDetails, Timetable
 from .matcher.utils import timer
 
 logger = Logger()
-db_client = TimetableDBClient()
 
 two_hours_secs = -7200
 
@@ -27,30 +27,37 @@ s3 = boto3.client("s3")
 @timer(logger)
 def get_avls_for_group_id(
     group_id: str,
-    worker_id: int,
+    process_conn: DuckDBPyConnection,
 ) -> Sequence[AVLRecord]:
-    with duckdb.connect(
-        "avl_timetable.db",
-        config={"access_mode": "READ_ONLY"},
-    ) as process_conn:
-        logger.info(
-            f"Worker id {worker_id}: got connection and getting avls for group id {group_id}",
-        )
-        return [
-            {
-                "recorded_at_time": str(recorded_at_time),
-                "response_timestamp": str(response_time_stamp),
-                "latitude": float(latitude),
-                "longitude": float(longitude),
-                "line_name": str(line_name),
-                "operator_ref": str(operator_ref),
-                "vehicle_ref": str(vehicle_ref),
-                "journey_ref": str(journey_ref),
-                "direction_ref": str(direction_ref),
-                "date_of_journey": str(date_of_journey),
-                "batch_id": int(batch_id),
-            }
-            for (
+    return [
+        {
+            "recorded_at_time": str(recorded_at_time),
+            "response_timestamp": str(response_time_stamp),
+            "latitude": float(latitude),
+            "longitude": float(longitude),
+            "line_name": str(line_name),
+            "operator_ref": str(operator_ref),
+            "vehicle_ref": str(vehicle_ref),
+            "journey_ref": str(journey_ref),
+            "direction_ref": str(direction_ref),
+            "date_of_journey": str(date_of_journey),
+            "batch_id": int(batch_id),
+        }
+        for (
+            recorded_at_time,
+            response_time_stamp,
+            latitude,
+            longitude,
+            line_name,
+            operator_ref,
+            vehicle_ref,
+            journey_ref,
+            direction_ref,
+            date_of_journey,
+            batch_id,
+        ) in process_conn.execute(
+            f"""
+            SELECT
                 recorded_at_time,
                 response_time_stamp,
                 latitude,
@@ -61,53 +68,32 @@ def get_avls_for_group_id(
                 journey_ref,
                 direction_ref,
                 date_of_journey,
-                batch_id,
-            ) in process_conn.execute(
-                f"""
-                SELECT
-                    recorded_at_time,
-                    response_time_stamp,
-                    latitude,
-                    longitude,
-                    line_name,
-                    operator_ref,
-                    vehicle_ref,
-                    journey_ref,
-                    direction_ref,
-                    date_of_journey,
-                    batch_id
-                FROM avl
-                WHERE group_id = '{group_id}'
-                """,
-            ).fetchall()
-        ]
+                batch_id
+            FROM avl
+            WHERE group_id = '{group_id}'
+            """,
+        ).fetchall()
+    ]
 
 
 @timer(logger)
 def get_timetable_data_for_group_id(
     group_id: str,
-    worker_id: int,
+    process_conn: DuckDBPyConnection,
 ) -> Timetable:
-    with duckdb.connect(
-        "avl_timetable.db",
-        config={"access_mode": "READ_ONLY"},
-    ) as process_conn:
-        logger.info(
-            f"Worker id {worker_id}: got connection and getting timetable table for group id {group_id}",
-        )
-        stop_data = process_conn.execute(
-            f"""
-            SELECT
-                direction,
-                stop_latitude,
-                stop_longitude,
-                expected_departure_time,
-                timetable_id,
-                date_of_journey
-            FROM timetable
-            WHERE group_id = '{group_id}'
-            """,
-        ).fetchall()
+    stop_data = process_conn.execute(
+        f"""
+        SELECT
+            direction,
+            stop_latitude,
+            stop_longitude,
+            expected_departure_time,
+            timetable_id,
+            date_of_journey
+        FROM timetable
+        WHERE group_id = '{group_id}'
+        """,
+    ).fetchall()
     directions = {rec[0] for rec in stop_data}
     timetable: dict[str, dict[str, StopDetails]] = {}
     for (
@@ -150,108 +136,94 @@ def historic_matching(avl_path: str, timetable_path: str, date_str: str) -> None
         date_str (str): The date for historic matching
 
     """
-    num_workers = 8
     group_queue = Queue()
     with duckdb.connect("avl_timetable.db") as conn:
         conn.execute(f"CREATE OR REPLACE TABLE avl as SELECT * FROM '{avl_path}'")  # noqa: S608 Not really sql injection
         conn.execute(
             f"CREATE OR REPLACE TABLE timetable as SELECT * FROM '{timetable_path}'",  # noqa: S608 Not really sql injection
         )
-        group_ids: list[str] = [
-            row[0]
-            for row in conn.query(
-                """
-                SELECT DISTINCT a.group_id
-                FROM timetable t
-                INNER JOIN avl a ON a.group_id = t.group_id
-                """,
-            ).fetchall()
-        ]
-    number_of_groups = len(group_ids)
+
+        for row in conn.query(
+            """
+            SELECT DISTINCT a.group_id
+            FROM timetable t
+            INNER JOIN avl a ON a.group_id = t.group_id
+            """,
+        ).fetchall():
+            group_queue.put(row[0])
+    number_of_groups = (
+        group_queue.qsize()
+    )  # Should be fine since nothing is reading yet
     logger.info("Starting to process AVL data", number_of_groups=number_of_groups)
-    for group_id in group_ids:
-        group_queue.put(group_id)
-    logger.info(f"Launching {num_workers} workers")
-    workers = create_workers(date_str, group_ids, group_queue, num_workers)
+    workers = []
+    num_workers = 2
+    logger.info("Launching workers", num_workers=num_workers)
+    for i in range(num_workers):
+        worker = Process(
+            target=worker_task,
+            args=(date_str, number_of_groups, group_queue, i),
+        )
+        worker.start()
+        workers.append(worker)
     for worker in workers:
         worker.join()
 
 
-def process_group_data(
+def worker_task(
     date_str: str,
-    group_ids: list,
+    number_of_groups: int,
     group_queue: Queue,
     worker_id: int,
 ) -> None:
-    number_of_groups = len(group_ids)
     worker_count = 0
-    while True:
-        try:
-            group_id = group_queue.get(timeout=10)
-            logger.debug(f"Worker id {worker_id}: trying for group_id {group_id}")
-            if group_id is None:
-                break
-            logger.debug(
-                f"Worker id {worker_id}: getting sirivm timetable_query from {group_id}",
-            )
-            group_avls = get_avls_for_group_id(group_id, worker_id)
-            logger.info("Produced avl list", size=len(group_avls))
-            routes_for_group_id = get_timetable_data_for_group_id(
-                group_id,
-                worker_id,
-            )
-            logger.info(
-                "Processing group_id",
-                group_id=group_id,
-                group_number=worker_count,
-                number_of_groups=number_of_groups,
-            )
-            timetable_store = LiveTimetableStore(routes_for_group_id)
-            total_to_set = []
-            stop_history = {}
-            for avl in group_avls:
-                to_set, to_remove, stop_history = positions_timetable_lookup(
-                    timetable_store,
-                    [avl],
-                    stop_history,
-                )
-                remove_timetable_ids = [rec["timetable_id"] for rec in to_remove]
-                total_to_set = [
-                    rec
-                    for rec in total_to_set
-                    if rec["timetable_id"] not in remove_timetable_ids
-                ]
-                total_to_set.extend(to_set)
-            db_client.historic_update_success(
-                None,
-                total_to_set,
-                [],
-                date_str,
-            )
-            worker_count += 1
-            if worker_count % 100 == 0:
+    logger.append_keys(worker_id=worker_id)
+    db_client = TimetableDBClient()
+    with duckdb.connect(
+        "avl_timetable.db",
+        config={"access_mode": "READ_ONLY"},
+    ) as process_conn:
+        while True:
+            try:
+                group_id = group_queue.get(timeout=10)
+                if group_id is None:
+                    return
                 logger.info(
-                    f"Worker id {worker_id}: Processed {worker_count} group_ids",
+                    "Processing group_id",
+                    group_id=group_id,
+                    group_number=worker_count,
+                    initial_group_count=number_of_groups,
+                    estimated_remaining_groups=group_queue.qsize(),
                 )
-        except Exception:
-            logger.exception("An error occurred when processing historic record")
-
-
-def create_workers(
-    date_str: str,
-    group_ids: list,
-    group_queue: Queue,
-    num_workers: int,
-) -> list[Process]:
-    workers = []
-    for i in range(num_workers):
-        worker = Process(
-            target=process_group_data,
-            args=(date_str, group_ids, group_queue, i),
-        )
-        worker.start()
-        workers.append(worker)
-    return workers
+                group_avls = get_avls_for_group_id(group_id, process_conn)
+                timetable_store = LiveTimetableStore(
+                    get_timetable_data_for_group_id(
+                        group_id,
+                        process_conn,
+                    )
+                )
+                total_to_set = []
+                stop_history = {}
+                for avl in group_avls:
+                    to_set, to_remove, stop_history = positions_timetable_lookup(
+                        timetable_store,
+                        [avl],
+                        stop_history,
+                    )
+                    remove_timetable_ids = [rec["timetable_id"] for rec in to_remove]
+                    total_to_set = [
+                        rec
+                        for rec in total_to_set
+                        if rec["timetable_id"] not in remove_timetable_ids
+                    ]
+                    total_to_set.extend(to_set)
+                db_client.historic_update_success(
+                    None,
+                    total_to_set,
+                    [],
+                    date_str,
+                )
+            except Exception:
+                logger.exception("An error occurred when processing historic record")
 
 
 if __name__ == "__main__":
