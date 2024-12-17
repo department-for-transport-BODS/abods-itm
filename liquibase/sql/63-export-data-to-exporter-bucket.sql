0659b07784dc65a5f2e1f485035b596b4e92715f
Gareth Somerville <gareth.somerville@doza.consulting>
2024-12-17 11:32:43 +0000

    Tidy up

--- liquibase/sql/63-export-data-to-exporter-bucket.sql
+++ liquibase/sql/63-export-data-to-exporter-bucket.sql
@@ -1,74 +1,81 @@
-CREATE OR REPLACE PROCEDURE public.historic_timetable_export(IN partition_date date)
-    LANGUAGE plpgsql
-AS
+CREATE OR REPLACE FUNCTION public.environment_name() RETURNS TEXT
+RETURN SPLIT_PART(aurora_db_instance_identifier(), '-', 2);
+
+
+CREATE OR REPLACE PROCEDURE public.historic_timetable_export(IN partition_date DATE)
+    LANGUAGE PLPGSQL AS
 $procedure$
+DECLARE
+    datestring TEXT := to_char(partition_date, 'YYYY-MM-DD');
+BEGIN
+    RAISE NOTICE 'Exporting timetable for date %', partition_date::TEXT;
 
-declare
-    datestring text := to_char(partition_date, 'YYYY-MM-DD');
-    yearstr    text := DATE_PART('year', partition_date);
-    monthstr   text := DATE_PART('month', partition_date);
+    SELECT *
+    FROM aws_s3.query_export_to_s3(
+            format('SELECT group_id,
+                           row_number() OVER (PARTITION BY group_id
+                                              ORDER BY group_id, expected_departure_time ASC, stop_index ASC) AS stop_index,
+                           stop_latitude,
+                           stop_longitude,
+                           expected_departure_time::TIME AS expected_departure_time,
+                           timetable_id,
+                           date_of_journey,
+                           direction,
+                           operator_noc
+                    FROM public."Timetable"
+                    WHERE date_of_journey = ''%s''::DATE
+                    ORDER BY group_id ASC,
+                             expected_departure_time ASC,
+                             stop_index ASC', datestring),
+            aws_commons.create_s3_uri(
+                    concat('abods-', public.environment_name(), '-exporter-bucket'),
+                    concat(
+                            'historic/csv/timetable/YYYY=',
+                            DATE_PART('year', partition_date),
+                            '/MM=',
+                            DATE_PART('month', partition_date),
+                            '/',
+                            datestring,
+                            '.csv'
+                    ),
+                    'eu-west-2'
+            ),
+            options := 'format csv'
+         );
 
-begin
+    RAISE NOTICE 'Exported timetable for date %', partition_date::TEXT;
+END;
+$procedure$;
 
-    RAISE NOTICE 'Exporting timetable for date %', partition_date::text;
-    execute format(
-            'SELECT * from aws_s3.query_export_to_s3(
-                ''select
-                    group_id,
-                    row_number() over( partition by group_id order by group_id,expected_departure_time asc, stop_index  asc  ) as stop_index,
-                    stop_latitude,
-                    stop_longitude,
-                    expected_departure_time::time as expected_departure_time,
-                    timetable_id,
-                    date_of_journey,
-                    direction,
-                    operator_noc
-                from
-                    public."Timetable"
-                where
-                    date_of_journey  = ''''%s''''::date
-                order by
-                    group_id asc,
-                    expected_departure_time asc,
-                    stop_index  asc
-                '',
-                aws_commons.create_s3_uri(concat(''abods-'', SPLIT_PART(aurora_db_instance_identifier(), ''-'', 2), ''-exporter-bucket''), ''historic/csv/timetable/YYYY=%s/MM=%s/%s.csv'',  ''eu-west-2''),
-                options :=''format csv''
-            )',
-            datestring,
-            yearstr,
-            monthstr,
-            datestring
-            );
-    RAISE NOTICE 'Exported timetable for date %', partition_date::text;
-end;
+
+CREATE OR REPLACE PROCEDURE public.historic_avl_export(IN partition_date DATE)
+    LANGUAGE PLPGSQL AS
 $procedure$
-;
+DECLARE
+    datestring TEXT := to_char(partition_date, 'YYYY-MM-DD');
+BEGIN
+    RAISE NOTICE 'Exporting sirivmpositions for date %', partition_date::TEXT;
 
+    SELECT *
+    FROM aws_s3.query_export_to_s3(
+            format('SELECT *
+                    FROM public."SiriVMPositions"
+                    WHERE date_of_journey = ''%s''::DATE', datestring),
+            aws_commons.create_s3_uri(
+                    concat('abods-', public.environment_name(), '-exporter-bucket'),
+                    concat(
+                            'historic/csv/siri/YYYY=',
+                            DATE_PART('year', partition_date),
+                            '/MM=',
+                            DATE_PART('month', partition_date),
+                            '/siri_vm_',
+                            datestring,
+                            '.csv'
+                    ),
+                    'eu-west-2'),
+            options := 'format csv'
+         );
 
-CREATE OR REPLACE PROCEDURE public.historic_avl_export(IN partition_date date)
-    LANGUAGE plpgsql
-AS
-$procedure$
-
-declare
-    datestring text := to_char(partition_date, 'YYYY-MM-DD');
-    yearstr    text := DATE_PART('year', partition_date);
-    monthstr   text := DATE_PART('month', partition_date);
-
-begin
-
-    execute format(
-            'SELECT * from aws_s3.query_export_to_s3(
-            ''select * from public."SiriVMPositions" where date_of_journey  = ''''%s''''::date'',
-            aws_commons.create_s3_uri(concat(''abods-'', SPLIT_PART(aurora_db_instance_identifier(), ''-'', 2), ''-exporter-bucket''), ''historic/csv/siri/YYYY=%s/MM=%s/siri_vm_%s.csv'',  ''eu-west-2''),
-        options :=''format csv''
-        )',
-            datestring,
-            yearstr,
-            monthstr,
-            datestring
-            );
-end;
-$procedure$
-;
+    RAISE NOTICE 'Exported sirivmpositions for date %', partition_date::TEXT;
+END;
+$procedure$;
