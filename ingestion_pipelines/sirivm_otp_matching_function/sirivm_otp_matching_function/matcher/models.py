de810864d869fcd7603c6d425fe09fc576baef70
Gareth Somerville <gareth.somerville@doza.consulting>
2024-12-30 14:14:51 +0000

    Fix live matching (#251)
    * Fix live matching failing to read avl files
    
    * Update version
    
    * Clarify things

--- ingestion_pipelines/sirivm_otp_matching_function/sirivm_otp_matching_function/matcher/models.py
+++ ingestion_pipelines/sirivm_otp_matching_function/sirivm_otp_matching_function/matcher/models.py
@@ -56,7 +56,20 @@ class LiveAVLRecord(AVLRecord):
     batch_id: int
 
 
-avl_data_type = AVLRecord.__annotations__
+# Live avl files contain more records than we need, and the ordering is important so defined here to be explicit
+live_avl_file_columns = {
+    "recorded_at_time": str,
+    "response_timestamp": str,
+    "latitude": float,
+    "longitude": float,
+    "line_name": str,
+    "operator_ref": str,
+    "vehicle_ref": str,
+    "journey_ref": str,
+    "direction_ref": str,
+    "date_of_journey": str,
+    "batch_id": int,
+}
 
 
 def avl_group_id(avl: AVLRecord) -> str:
