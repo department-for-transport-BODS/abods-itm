7fdd941a6c0ddc4e171d6c3981ab7522eed15595
Gareth Somerville <gareth.somerville@doza.consulting>
2025-01-14 12:23:10 +0000

    Changes to module paths in test files (#276)

--- ingestion_pipelines/sirivm_otp_matching_function/sirivm_otp_matching_function/matcher/test_data/get_test_data.py
+++ ingestion_pipelines/sirivm_otp_matching_function/sirivm_otp_matching_function/matcher/test_data/get_test_data.py
@@ -5,11 +5,7 @@ from pathlib import Path
 
 import pandas as pd
 
-from ingestion_pipelines.sirivm_otp_matching_function.sirivm_otp_matching_function.matcher.models import (
-    AVLRecord,
-    Timetable,
-    live_avl_file_columns,
-)
+from ..models import AVLRecord, Timetable, live_avl_file_columns
 
 test_data_dir = Path(__file__).parent
 
