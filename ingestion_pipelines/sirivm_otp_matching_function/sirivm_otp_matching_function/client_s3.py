2306dc3c5de93bd6844621f5a1e94185a54eba42
Gareth Somerville <gareth.somerville@doza.consulting>
2025-01-15 09:51:18 +0000

    Reapply "Simplify avl data fetching (#273)" (#279)
    This reverts commit 3c1b0088ad103c2056fb0e257dec7efbe2ef2f42.

--- ingestion_pipelines/sirivm_otp_matching_function/sirivm_otp_matching_function/client_s3.py
+++ ingestion_pipelines/sirivm_otp_matching_function/sirivm_otp_matching_function/client_s3.py
@@ -1,30 +1,30 @@
 """Fetching and Uploading Data into S3"""
 
+import codecs
 import hashlib
 import json
 import os
-import time
 from collections.abc import Generator, Sequence
 from typing import Any
 
-import awswrangler as wr
 import boto3
 from aws_lambda_powertools import Logger
 from botocore.exceptions import ClientError
-from pandas import DataFrame
+from botocore.response import StreamingBody
 
 from .matcher.models import (
     LiveAVLRecord,
     OperatorShards,
     StopHistory,
     Timetable,
-    live_avl_file_columns,
+    parse_live_avl_data,
 )
 from .matcher.utils import timer
 from .shards import shards
 
 logger = Logger()
 client = boto3.client("s3")
+utf8_stream_reader = codecs.getreader("utf-8")
 shard_lookup: dict[str, str] = {}
 for shard, operators in shards.items():
     for operator in operators:
@@ -58,20 +58,11 @@ class TimetableS3Client:
         self.bucket = os.environ["SIRIVM_BUCKET"]
         logger.append_keys(s3_bucket=self.bucket)
 
-    def _get_from_s3(self, key: str) -> Any:  # noqa: ANN401 - Any is correct here, callers should determine actual type
+    def _get_from_s3(self, key: str) -> StreamingBody:
         """Get data from S3"""
         try:
             logger.info("Fetching data from S3", s3_key=key)
-            content = (
-                self.client.get_object(Bucket=self.bucket, Key=key).get("Body").read()
-            )
-            size = round(len(content) / (1024), 2)
-            logger.info(
-                "Successfully fetched data from S3",
-                s3_key=key,
-                size_kb=size,
-            )
-            return json.loads(content)
+            return self.client.get_object(Bucket=self.bucket, Key=key).get("Body")
         except ClientError as e:
             error_code = e.response.get("Error", {}).get("Code", "Unknown")
             logger.exception(
@@ -98,12 +89,12 @@ class TimetableS3Client:
 
     def download_main_timetable(self) -> Timetable:
         """Download Main Timetable Data"""
-        return self._get_from_s3("timetable/timetable.json")
+        return json.load(self._get_from_s3("timetable/timetable.json"))
 
     @timer(logger)
     def get_shards(self) -> OperatorShards:
         """Get Shard Data from S3 and return Shards Model"""
-        return self._get_from_s3("shards.json")["shards"]
+        return json.load(self._get_from_s3("shards.json"))["shards"]
 
     @timer(logger)
     def get_stop_history(self, shard_no: str) -> StopHistory:
@@ -111,7 +102,7 @@ class TimetableS3Client:
         key = stop_history_key(shard_no)
         logger.info("Fetching Stop History", s3_key=key)
         try:
-            stop_history = self._get_from_s3(key)
+            stop_history = json.load(self._get_from_s3(key))
         except ClientError as ex:
             if ex.response.get("Error", {}).get("Code", None) == "NoSuchKey":
                 logger.info("Stop History Not Found, Returning Empty Dict", key=key)
@@ -127,42 +118,12 @@ class TimetableS3Client:
 
         return stop_history
 
-    def get_avl_data_df(self, filename: str | list[str]) -> DataFrame:
-        """Get AVL Data Dataframe"""
-        paths: list[str] = []
-        if isinstance(filename, list):
-            paths = [f"s3://{self.bucket}/{path}" for path in filename]
-        else:
-            paths = [f"s3://{self.bucket}/{filename}"]
-        logger.info("Getting AVL Data", path=filename)
-        logger.info(
-            "Fetching AVL Data can take a while...",
-            number_of_files=len(filename),
-        )
-        start_time = time.time()
-        keys = list(live_avl_file_columns.keys())
-        data = wr.s3.read_csv(
-            path=paths,
-            use_threads=True,
-            names=keys,
-            dtype=live_avl_file_columns,
-            usecols=keys,
-            header=None,
-        )
-        data["line_name"] = data["line_name"].fillna("")
-        data["direction_ref"] = data["direction_ref"].fillna("")
-        logger.info(
-            "AVL Downloaded and Parsed into DataFrame",
-            count=len(data),
-            processing_time=time.time() - start_time,
-        )
-        return data
-
     @timer(logger)
-    def get_avl_data(self, filename: str | list[str]) -> Sequence[LiveAVLRecord]:
+    def get_avl_data(self, filename: str) -> Sequence[LiveAVLRecord]:
         """Get AVL Data from S3 and return a list of AVLData models"""
-        data = self.get_avl_data_df(filename)
-        return data.to_dict("records")
+        logger.info("Getting AVL Data", path=filename)
+        stream = utf8_stream_reader(self._get_from_s3(filename))
+        return parse_live_avl_data(stream)
 
     @timer(logger)
     def export_stop_history(self, stop_history: StopHistory, shard_no: str) -> None:
--- ingestion_pipelines/sirivm_otp_matching_function/sirivm_otp_matching_function/matcher/historic_examples/util.py
+++ ingestion_pipelines/sirivm_otp_matching_function/sirivm_otp_matching_function/matcher/historic_examples/util.py
@@ -1,40 +1,12 @@
-import csv
 import json
 import os
 import pathlib
-from collections.abc import Iterable, Sequence
+from collections.abc import Sequence
 from unittest import mock
 
 from ..live_timetable_store import LiveTimetableStore
 from ..matching import match_group_id_avls
-from ..models import LiveAVLRecord, NewDbMatch
-
-# Live avl files contain more records than we need, and the ordering is important so defined here to be explicit
-live_avl_file_columns = {
-    "recorded_at_time": str,
-    "response_timestamp": str,
-    "latitude": float,
-    "longitude": float,
-    "line_name": lambda x: str(x or ""),
-    "operator_ref": str,
-    "vehicle_ref": str,
-    "journey_ref": str,
-    "direction_ref": lambda x: str(x or ""),
-    "date_of_journey": str,
-    "batch_id": int,
-}
-fieldnames = list(live_avl_file_columns)
-
-
-def parse_live_avl_data(stream: Iterable[str]) -> Sequence[LiveAVLRecord]:
-    """Parse live avl csv data into LiveAVLRecord dicts"""
-    rows = []
-    for row in csv.DictReader(stream):
-        for key, val in row.items():
-            convert = live_avl_file_columns.get(key) or str
-            row[key] = convert(val)
-        rows.append(row)
-    return rows
+from ..models import NewDbMatch, parse_live_avl_data
 
 
 def run_historic_matching_test(
@@ -45,7 +17,7 @@ def run_historic_matching_test(
     with open(directory / "timetable.json") as f:
         timetable = json.load(f)
     with open(directory / "avl.csv") as csvfile:
-        avl_list = parse_live_avl_data(csvfile)
+        avl_list = parse_live_avl_data(csvfile, has_header=True)
 
     with mock.patch.dict(os.environ, {"ENABLE_ESTIMATED_MATCHING": "true"}):
         to_set, _, __ = match_group_id_avls(LiveTimetableStore(timetable), avl_list)
