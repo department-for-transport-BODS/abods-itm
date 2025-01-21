6c1f0dbb30c693718b83e8118528ddfd025ba1b5
Gareth Somerville <gareth.somerville@doza.consulting>
2025-01-21 17:18:33 +0000

    Make column nullable

--- liquibase/sql/114-thresholds-departure-hour-only.sql
+++ liquibase/sql/114-thresholds-departure-hour-only.sql
@@ -1,2 +1,2 @@
-ALTER TABLE abods.public.timetable_threshold_summary
-    ADD COLUMN IF NOT EXISTS departure_hour_only time with time zone NOT NULL default date_of_journey::date || ' ' || departure_hour::time::text;
+ALTER TABLE public.timetable_threshold_summary
+    ADD COLUMN IF NOT EXISTS departure_hour_only time with time zone;
