61763876107268488a57adf59c3f16401e7ab999
Karthik Alva <karthik.alva@kpmg.co.uk>
2025-01-24 16:14:47 +0000

    fixed the sql to be compatible in psql 16

--- liquibase/procedures/sync-tracks.sql
+++ liquibase/procedures/sync-tracks.sql
@@ -7,13 +7,14 @@ begin
     ON (target.from_atco_code = source.from_atco_code AND target.to_atco_code = source.to_atco_code)
     WHEN MATCHED THEN
         UPDATE SET
-            target.geometry = ST_AsGeoJSON(source.geometry),
-            target.distance = source.distance
-    WHEN NOT MATCHED BY TARGET THEN
+            geometry = ST_AsGeoJSON(source.geometry),
+            distance = source.distance
+    WHEN NOT MATCHED THEN
         INSERT (id, from_atco_code, to_atco_code, geometry, distance)
         VALUES (source.id, source.from_atco_code, source.to_atco_code, ST_AsGeoJSON(source.geometry), source.distance)
-    WHEN NOT MATCHED BY SOURCE THEN
-        DELETE;
+    
+    DELETE FROM public.transmodel_tracks
+    WHERE id NOT IN (SELECT id FROM bods.transmodel_tracks);
 
 end; $procedure$
 ;
