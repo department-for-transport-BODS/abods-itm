c9e51b0570fb32d6361d75d683197d592ba78844
Karthik Alva <karthik.alva@kpmg.co.uk>
2025-01-24 14:18:20 +0000

    synx transmodel tracks table from bods to abods

---
+++ liquibase/sql/115-import-tracks.sql
@@ -0,0 +1,19 @@
+IMPORT FOREIGN SCHEMA public LIMIT TO (
+    transmodel_tracks
+)
+FROM SERVER bods INTO bods;
+
+GRANT SELECT ON TABLE bods."transmodel_tracks" TO abods_rw;
+
+CREATE TABLE public.transmodel_tracks (
+	id bigint,
+	from_atco_code varchar(255) NOT NULL,
+	to_atco_code varchar(255) NOT NULL,
+	--geometry public.geometry(linestring, 4326) NULL,
+    geometry text NULL,
+	distance int4 NULL,
+	CONSTRAINT transmodel_tracks_pkey PRIMARY KEY (id),
+	CONSTRAINT unique_from_to_atco_code UNIQUE (from_atco_code, to_atco_code)
+);
+alter table public.transmodel_tracks owner to abods_rw;
+CREATE INDEX transmodel_tracks_geometry_idx ON public.transmodel_tracks USING btree (from_atco_code, to_atco_code);
---
+++ liquibase/sql/sync-tracks.sql
@@ -0,0 +1,21 @@
+create or replace procedure public.update_transmodel_servicepatternstop()
+ LANGUAGE plpgsql
+AS $procedure$
+begin
+    MERGE INTO public.transmodel_tracks AS target
+    USING bods.transmodel_tracks AS source
+    ON (target.from_atco_code = source.from_atco_code AND target.to_atco_code = source.to_atco_code)
+    WHEN MATCHED THEN
+        UPDATE SET
+            target.geometry = ST_AsGeoJSON(source.geometry),
+            target.distance = source.distance
+    WHEN NOT MATCHED BY TARGET THEN
+        INSERT (id, from_atco_code, to_atco_code, geometry, distance)
+        VALUES (source.id, source.from_atco_code, source.to_atco_code, ST_AsGeoJSON(source.geometry), source.distance)
+    WHEN NOT MATCHED BY SOURCE THEN
+        DELETE;
+
+end; $procedure$
+;
+
+alter procedure public.transmodel_tracks owner to abods_rw;
