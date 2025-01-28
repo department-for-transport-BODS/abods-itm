f2c8ae62504907a2e65f407688a966a297595289
levina-c <levina.chan@kpmg.co.uk>
2025-01-28 10:11:53 +0000

    remove 2 hours range in set matching sql scripts

--- ingestion_pipelines/sirivm_otp_matching_function/sirivm_otp_matching_function/sql/set_live_matching.sql
+++ ingestion_pipelines/sirivm_otp_matching_function/sirivm_otp_matching_function/sql/set_live_matching.sql
@@ -7,9 +7,4 @@ SET time_difference = t.time_difference::int,
 FROM (VALUES %s) AS t(timetable_id, time_difference, last_time_in_zone_utc, otp_state, timestamp_after_estimate)
 WHERE u.timetable_id = t.timetable_id::bigint
   AND date_of_journey = now()::date
-  AND COALESCE(
-              EXTRACT(epoch FROM (t.last_time_in_zone_utc::timestamp AT TIME ZONE 'UTC' - u.expected_departure_time)),
-              EXTRACT(epoch FROM (t.timestamp_after_estimate::timestamp AT TIME ZONE 'UTC' - u.expected_departure_time)),
-              0
-      ) > -7200
 RETURNING u.timetable_id;
