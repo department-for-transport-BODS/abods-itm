51a5d01f38a67d0a7f9688cf828c7590213fd0fc
levina-c <78670725+levina-c@users.noreply.github.com>
2025-01-28 15:54:04 +0000

    Only update matches within 2 hours to db (#317)
    * remove 2 hours range in set matching sql scripts
    
    * add matching time lower limit
    
    * move get_time_difference function to matching
    
    * add test cases
    
    * add journey test case
    
    * remove matcher_config and return early for mapping matched stop to db
    
    * Update ingestion_pipelines/sirivm_otp_matching_function/sirivm_otp_matching_function/matcher/matching.py
    
    Co-authored-by: Gareth Somerville <gareth.somerville@doza.consulting>
    
    ---------
    
    Co-authored-by: Gareth Somerville <gareth.somerville@doza.consulting>

--- ingestion_pipelines/sirivm_otp_matching_function/sirivm_otp_matching_function/sql/set_historic_matching.sql
+++ ingestion_pipelines/sirivm_otp_matching_function/sirivm_otp_matching_function/sql/set_historic_matching.sql
@@ -15,9 +15,4 @@ SET time_difference          =
 FROM (VALUES %s) AS t(timetable_id, time_difference, last_time_in_zone_utc, is_final_stop, journey_date, timestamp_after_estimate)
 WHERE u.timetable_id = t.timetable_id::bigint
   AND date_of_journey = t.journey_date::date
-  AND COALESCE(
-      EXTRACT(epoch FROM (t.last_time_in_zone_utc::timestamp AT TIME ZONE 'UTC' - u.expected_departure_time)),
-      EXTRACT(epoch FROM (t.timestamp_after_estimate::timestamp AT TIME ZONE 'UTC' - u.expected_departure_time)),
-      0
-  ) > -7200
 RETURNING u.timetable_id;
