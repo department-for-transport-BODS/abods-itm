1f5a7491c45c14265603181df44110684c0fd9b7
Gareth Somerville <gareth.somerville@doza.consulting>
2025-01-31 10:56:31 +0000

    BODS-8002 Ingestion fixes (#335)
    * Stop writing to old queue
    
    * Better logging on errors writing to queues
    
    * Fix building queue names
    
    * Fix linting

--- ingestion_pipelines/sirivm_timetable_s3_generation_function/sirivm_timetable_s3_generation_function/app.py
+++ ingestion_pipelines/sirivm_timetable_s3_generation_function/sirivm_timetable_s3_generation_function/app.py
@@ -18,7 +18,7 @@ db_port = environ.get("POSTGRES_PORT")
 db_user = environ.get("POSTGRES_USER")
 db_database = environ.get("POSTGRES_DB")
 sirivm_bucket = environ.get("SIRIVM_BUCKET")
-otp_queue = environ.get("SIRIVM_OTP_QUEUE")
+otp_queue = environ.get("SIRIVM_OTP_QUEUE_PREFIX")
 logger = logging.getLogger("sirivm")
 logging.getLogger().setLevel("INFO")
 
@@ -50,12 +50,6 @@ def write_to_s3(data_dict, path):  # noqa: ANN001, ANN201 - BODS-7131
     data_string = json.dumps(data_dict, default=str)
 
     client.put_object(Bucket=sirivm_bucket, Key=path, Body=data_string)
-
-
-def getQueue(queue):  # noqa: ANN001, ANN201, N802 - BODS-7131
-    """Retrieve the URL for the configured queue name"""
-    q = sqs.get_queue_by_name(QueueName=queue)
-    return q
 
 
 def read_historic_timetable(timetable_date):  # noqa: ANN001, ANN201 - BODS-7131
@@ -263,30 +257,6 @@ def backfill_lambda_handler(event, context):  # noqa: ANN001, ANN201, ARG001, PL
 
 
 def live_lambda_handler(event, context):  # noqa: ANN001, ANN201, ARG001 - BODS-7131
-    query = """
-        WITH my_groups AS
-          (SELECT DISTINCT vehiclejourney_id
-           FROM public."Timetable"
-           WHERE date_of_journey = (now() AT TIME ZONE 'EUROPE/LONDON')::date
-             AND expected_departure_time BETWEEN current_timestamp(0) - interval '%s' MINUTE AND current_timestamp(0) + interval '%s' MINUTE)
-        SELECT t.group_id,
-               row_number() OVER (PARTITION BY t.vehiclejourney_id
-                                  ORDER BY t.group_id, t.expected_departure_time ASC, t.stop_index ASC) AS stop_index,
-               t.stop_latitude,
-               t.stop_longitude,
-               t.expected_departure_time::TIME AS expected_departure_time,
-               t.timetable_id,
-               t.date_of_journey,
-               t.direction
-        FROM public."Timetable" t
-        WHERE t.date_of_journey = (now() AT TIME ZONE 'EUROPE/LONDON')::date
-          AND t.vehiclejourney_id IN
-            (SELECT vehiclejourney_id
-             FROM my_groups)
-        ORDER BY t.group_id,
-                 t.expected_departure_time ASC,
-                 t.stop_index ASC;
-    """  # noqa: W291 - BODS-7131
     now = datetime.now()
     year = datetime.strftime(now, "%Y")
     mon = datetime.strftime(now, "%m")
@@ -296,53 +266,80 @@ def live_lambda_handler(event, context):  # noqa: ANN001, ANN201, ARG001 - BODS-
     minute_standardised = "30" if int(minute) >= 30 else "00"  # noqa: PLR2004 - BODS-7131
     fname = f"timetable_shreds/YYYY={year}/MM={mon}/DD={day}/timetable_{year}{mon}{day}_{hour}_{minute_standardised}.json"
 
-    try:
-        conn = psycopg2.connect(
-            host=db_host,
-            port=db_port,
-            database=db_database,
-            user=db_user,
-            password=get_rds_token(),
-            sslmode="require",
-        )
+    with psycopg2.connect(
+        host=db_host,
+        port=db_port,
+        database=db_database,
+        user=db_user,
+        password=get_rds_token(),
+        sslmode="require",
+    ) as conn:
         conn.autocommit = True
-        cur = conn.cursor()
-        interval_time = TIMETABLE_EXTRACT_SLIDING_WINDOW_TIME_IN_MINUTES
-        cur.execute(query, [interval_time, interval_time])
-        timetable_dict = defaultdict(dict)
-        res = cur.fetchall()
-        directions_by_group_id = {}
-        for i in res:
-            directions_by_group_id.setdefault(i[0], set()).add(i[7])
+        with conn.cursor() as cur:
+            interval_time = TIMETABLE_EXTRACT_SLIDING_WINDOW_TIME_IN_MINUTES
+            cur.execute(
+                """
+                    WITH my_groups AS
+                      (SELECT DISTINCT vehiclejourney_id
+                       FROM public."Timetable"
+                       WHERE date_of_journey = (now() AT TIME ZONE 'EUROPE/LONDON')::date
+                         AND expected_departure_time BETWEEN current_timestamp(0) - interval '%s' MINUTE AND current_timestamp(0) + interval '%s' MINUTE)
+                    SELECT t.group_id,
+                           row_number() OVER (PARTITION BY t.vehiclejourney_id
+                                              ORDER BY t.group_id, t.expected_departure_time ASC, t.stop_index ASC) AS stop_index,
+                           t.stop_latitude,
+                           t.stop_longitude,
+                           t.expected_departure_time::TIME AS expected_departure_time,
+                           t.timetable_id,
+                           t.date_of_journey,
+                           t.direction
+                    FROM public."Timetable" t
+                    WHERE t.date_of_journey = (now() AT TIME ZONE 'EUROPE/LONDON')::date
+                      AND t.vehiclejourney_id IN
+                        (SELECT vehiclejourney_id
+                         FROM my_groups)
+                    ORDER BY t.group_id,
+                             t.expected_departure_time ASC,
+                             t.stop_index ASC;
+                """,  # noqa: W291 - BODS-7131,
+                [interval_time, interval_time],
+            )
+            timetable_dict = defaultdict(dict)
+            res = cur.fetchall()
+    directions_by_group_id = {}
+    for i in res:
+        directions_by_group_id.setdefault(i[0], set()).add(i[7])
 
-        for i in res:
-            group_id = i[0]
-            directions = directions_by_group_id[group_id]
-            if len(directions) > 1:
-                direction = i[7]
-                group_id = group_id + "|" + direction
-            timetable_dict[group_id][i[1]] = [
-                (float(i[2]), float(i[3])),
-                i[4],
-                i[5],
-                i[6],
-            ]
-        cur.close()
-        write_to_s3(timetable_dict, "timetable/timetable.json")
-        write_to_s3(timetable_dict, fname)
-        for shard_no in range(no_of_shards):
-            queue_name = f"{otp_queue}{shard_no + 1}.fifo"
-            queue = getQueue(queue_name)
+    for i in res:
+        group_id = i[0]
+        directions = directions_by_group_id[group_id]
+        if len(directions) > 1:
+            direction = i[7]
+            group_id = group_id + "|" + direction
+        timetable_dict[group_id][i[1]] = [
+            (float(i[2]), float(i[3])),
+            i[4],
+            i[5],
+            i[6],
+        ]
+    write_to_s3(timetable_dict, "timetable/timetable.json")
+    write_to_s3(timetable_dict, fname)
+    for shard_no in range(no_of_shards):
+        group = f"{otp_queue}{shard_no + 1}"
+        queue_name = f"{group}.fifo"
+        try:
+            queue = sqs.get_queue_by_name(QueueName=queue_name)
             resp = queue.send_message(  # noqa: F841 - BODS-7131
                 MessageBody="Put gzip file to S3",
                 MessageDeduplicationId=str(uuid.uuid4()),
-                MessageGroupId=f"{queue_name.split('.')[0]}-group",
+                MessageGroupId=f"{group}-group",
                 MessageAttributes={
                     "key": {"StringValue": "timetable", "DataType": "String"},
                 },
             )
-            logging.info(
-                f"Send message to  {otp_queue}{shard_no + 1} so timetable is refreshed.",
-            )
-    except Exception as e:
-        logging.exception(e)  # noqa: TRY401 - BODS-7131
+        except Exception:
+            logging.exception(f"Failed to write to queue {queue_name}")
+            raise
+        logging.info(
+            f"Send message to  {otp_queue}{shard_no + 1} so timetable is refreshed.",
+        )
