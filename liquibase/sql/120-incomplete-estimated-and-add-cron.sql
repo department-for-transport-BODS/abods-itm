949d7b1212fb3d2ce217d94cc53a0a4030d0ea8a
Gareth Somerville <gareth.somerville@doza.consulting>
2025-02-14 17:37:42 +0000

    A few small changes

--- liquibase/sql/120-incomplete-estimated-and-add-cron.sql
+++ liquibase/sql/120-incomplete-estimated-and-add-cron.sql
@@ -1,1 +1,5 @@
-SELECT cron.schedule('Update headway & SiriVMposition id',  '00 01 * * *',  $$call public.populate_headway(now()::date - 1);call public.incomplete_data_load$$);
+SELECT cron.schedule(
+               'Update headway & SiriVMposition id',
+               '00 01 * * *',
+               $$call public.populate_headway(now()::date - 1);call public.incomplete_data_load$$
+       );
