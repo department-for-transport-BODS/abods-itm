faf517432404bc6c0ca859dfbd0cbb0673ea23db
Gareth Somerville <gareth.somerville@doza.consulting>
2025-02-17 16:23:54 +0000

    Reapply change after accidental revert

--- shared/config.py
+++ shared/config.py
@@ -1,5 +1,5 @@
 TIMETABLE_EXTRACT_SLIDING_WINDOW_TIME_IN_MINUTES = 2 * 60
 "Period of time that the timetable extract for live matching should cover"
 
-EXPECTED_LATE_RUNNING_SERVICE_INTERVAL_IN_MINUTES = 4 * 60
+EXPECTED_LATE_RUNNING_SERVICE_INTERVAL_IN_MINUTES = 12 * 60
 """Maximum amount of time that late running services are expected to continue past midnight"""
