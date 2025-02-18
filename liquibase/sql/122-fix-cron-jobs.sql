4900ee638eac2fd7abbb6d289b1ec27224278fa3
Gareth Somerville <gareth.somerville@doza.consulting>
2025-02-18 13:27:35 +0000

    Sort out other jobs

--- liquibase/sql/122-fix-cron-jobs.sql
+++ liquibase/sql/122-fix-cron-jobs.sql
@@ -4,3 +4,16 @@ SELECT cron.schedule(
                $$call public.populate_headway(now()::date - 1);call public.incomplete_data_load(now()::date - 1);$$
        );
 SELECT cron.unschedule('create_partition_summary_service');
+SELECT cron.unschedule('create_partition_operators_if_not_exists');
+
+-- redoing jobs that already exist with no jobname. To make things easier, existing one will be removed manually on deploy
+SELECT cron.schedule(
+               'partman.run_maintenance_proc',
+               '15 23 * * *',
+               $$CALL partman.run_maintenance_proc()$$
+       );
+SELECT cron.schedule(
+               'partman.partition_data_proc',
+               '00 22 * * *',
+               $$CALL partman.partition_data_proc('public.SiriVMPositions')$$
+       );
