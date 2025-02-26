c6fad28ee8b20831ad9caf099212f0f2a3ca8c43
Gareth Somerville <gareth.somerville@doza.consulting>
2025-02-26 10:08:30 +0000

    Use temp tables and no formatting

--- liquibase/procedures/incomplete_data_load.sql
+++ liquibase/procedures/incomplete_data_load.sql
@@ -3,146 +3,120 @@ create or replace procedure incomplete_data_load(IN partition_date date DEFAULT 
 as
 $$
 DECLARE
-    longdatestring TEXT := to_char(partition_date, 'YYYY_MM_DD');
+    next_date            DATE     = partition_date + INTERVAL '1' DAY;
+    recent_stop_interval INTERVAL = INTERVAL '120' MINUTE;
 BEGIN
     RAISE NOTICE '% Start running incomplete_data_load ', clock_timestamp();
 
     RAISE NOTICE '% Creating stops without operator nocs table', clock_timestamp();
 
-    EXECUTE format('
-                    CREATE TABLE public.%I AS
-                    SELECT
-                      timetable_id
-                    FROM
-                      public."Timetable" t
-                    WHERE
-                      date_of_journey = %L
-                      AND expected_departure_time < (now() - interval ''120'' MINUTE)
-                      AND actual_departure_time IS NULL
-                      AND NOT EXISTS (
-                        SELECT
-                          1
-                        FROM
-                          public."Timetable" t2
-                          JOIN public."SiriVMPositions" s ON t2.operator_noc = s.operator_ref
-                        WHERE
-                          t2.timetable_id = t.timetable_id
-                          AND t2.date_of_journey = %L
-                          AND (s.date_of_journey = %L OR s.date_of_journey = (%L + interval ''1'' DAY))
-                      );',
-                   concat('stops_wo_nocs_', longdatestring),
-                   partition_date,
-                   partition_date,
-                   partition_date,
-                   partition_date
-            );
+    CREATE TEMP TABLE incomplete_data_tmp_stops_wo_nocs AS
+    SELECT
+      timetable_id
+    FROM
+      public."Timetable" t
+    WHERE
+      date_of_journey = partition_date
+      AND expected_departure_time < (now() - recent_stop_interval)
+      AND actual_departure_time IS NULL
+      AND NOT EXISTS (
+        SELECT
+          1
+        FROM
+          public."Timetable" t2
+          JOIN public."SiriVMPositions" s ON t2.operator_noc = s.operator_ref
+        WHERE
+          t2.timetable_id = t.timetable_id
+          AND t2.date_of_journey = partition_date
+          AND (s.date_of_journey = partition_date OR s.date_of_journey = next_date)
+      );
 
     RAISE NOTICE '% Created stops without operator nocs table', clock_timestamp();
 
     RAISE NOTICE '% Creating stops without journey code table', clock_timestamp();
 
-    EXECUTE format('
-                    CREATE TABLE public.%I AS
-                    SELECT
-                      timetable_id
-                    FROM
-                      public."Timetable" t
-                    WHERE
-                      date_of_journey = %L
-                      AND actual_departure_time IS NULL
-                      AND expected_departure_time < (now() - interval ''120'' MINUTE)
-                      AND NOT EXISTS (
-                        SELECT
-                          1
-                        FROM
-                          public."Timetable" t2
-                          JOIN public."SiriVMPositions" s
-                            ON t.operator_noc = s.operator_ref
-                           AND t.line_name = s.line_name
-                           AND t.journey_code = s.journey_ref
-                        WHERE
-                          t2.timetable_id = t.timetable_id
-                          AND t2.date_of_journey = %L
-                          AND (s.date_of_journey = %L OR s.date_of_journey = (%L + interval ''1'' DAY))
-                      )
-                      AND EXISTS (
-                        SELECT
-                          1
-                        FROM
-                          public."Timetable" t3
-                          JOIN public."SiriVMPositions" s ON t3.operator_noc = s.operator_ref
-                          AND t3.line_name = s.line_name
-                        WHERE
-                          t3.timetable_id = t.timetable_id
-                          AND t3.date_of_journey = %L
-                          AND (s.date_of_journey = %L OR s.date_of_journey = (%L + interval ''1'' DAY))
-                      );',
-                   concat('stops_wo_journey_code_', longdatestring),
-                   partition_date,
-                   partition_date,
-                   partition_date,
-                   partition_date,
-                   partition_date,
-                   partition_date,
-                   partition_date
-            );
+    CREATE TEMP TABLE incomplete_data_tmp_stops_wo_journey_code AS
+    SELECT
+      timetable_id
+    FROM
+      public."Timetable" t
+    WHERE
+      date_of_journey = partition_date
+      AND actual_departure_time IS NULL
+      AND expected_departure_time < (now() - recent_stop_interval)
+      AND NOT EXISTS (
+        SELECT
+          1
+        FROM
+          public."Timetable" t2
+          JOIN public."SiriVMPositions" s
+            ON t.operator_noc = s.operator_ref
+           AND t.line_name = s.line_name
+           AND t.journey_code = s.journey_ref
+        WHERE
+          t2.timetable_id = t.timetable_id
+          AND t2.date_of_journey = partition_date
+          AND (s.date_of_journey = partition_date OR s.date_of_journey = next_date)
+      )
+      AND EXISTS (
+        SELECT
+          1
+        FROM
+          public."Timetable" t3
+          JOIN public."SiriVMPositions" s ON t3.operator_noc = s.operator_ref
+          AND t3.line_name = s.line_name
+        WHERE
+          t3.timetable_id = t.timetable_id
+          AND t3.date_of_journey = partition_date
+          AND (s.date_of_journey = partition_date OR s.date_of_journey = next_date)
+      );
 
     RAISE NOTICE '% Created stops without journey code table', clock_timestamp();
 
     RAISE NOTICE '% Creating stops without service table', clock_timestamp();
 
-    EXECUTE format('
-                    CREATE TABLE public.%I AS
-                    SELECT
-                      timetable_id
-                    FROM
-                      public."Timetable" t
-                    WHERE
-                      date_of_journey = %L
-                      AND actual_departure_time IS NULL
-                      AND expected_departure_time < (now() - interval ''120'' MINUTE)
-                      AND NOT EXISTS (
-                        SELECT
-                          1
-                        FROM
-                          public."Timetable" t2
-                          JOIN public."SiriVMPositions" s
-                            ON t.operator_noc = s.operator_ref
-                           AND t.line_name = s.line_name
-                           AND t.journey_code = s.journey_ref
-                        WHERE
-                          t2.timetable_id = t.timetable_id
-                          AND t2.date_of_journey = %L
-                          AND (s.date_of_journey = %L OR s.date_of_journey = (%L + interval ''1'' DAY))
-                      )
-                      AND NOT EXISTS (
-                        SELECT
-                          1
-                        FROM
-                          public.%I
-                        WHERE
-                          timetable_id = t.timetable_id
-                      )
-                      AND EXISTS (
-                        SELECT
-                          1
-                        FROM
-                          public."Timetable" t3
-                          JOIN public."SiriVMPositions" s ON t3.operator_noc = s.operator_ref
-                        WHERE
-                          t3.timetable_id = t.timetable_id
-                          AND t3.date_of_journey = %L
-                          AND s.date_of_journey = %L
-                      );',
-                   concat('stops_wo_service_', longdatestring),
-                   partition_date,
-                   partition_date,
-                   partition_date,
-                   partition_date,
-                   concat('stops_wo_journey_code_', longdatestring),
-                   partition_date,
-                   partition_date
-            );
+    CREATE TEMP TABLE incomplete_data_tmp_stops_wo_service AS
+    SELECT
+      timetable_id
+    FROM
+      public."Timetable" t
+    WHERE
+      date_of_journey = partition_date
+      AND actual_departure_time IS NULL
+      AND expected_departure_time < (now() - recent_stop_interval)
+      AND NOT EXISTS (
+        SELECT
+          1
+        FROM
+          public."Timetable" t2
+          JOIN public."SiriVMPositions" s
+            ON t.operator_noc = s.operator_ref
+           AND t.line_name = s.line_name
+           AND t.journey_code = s.journey_ref
+        WHERE
+          t2.timetable_id = t.timetable_id
+          AND t2.date_of_journey = partition_date
+          AND (s.date_of_journey = partition_date OR s.date_of_journey = next_date)
+      )
+      AND NOT EXISTS (
+        SELECT
+          1
+        FROM
+          incomplete_data_tmp_stops_wo_journey_code
+        WHERE
+          timetable_id = t.timetable_id
+      )
+      AND EXISTS (
+        SELECT
+          1
+        FROM
+          public."Timetable" t3
+          JOIN public."SiriVMPositions" s ON t3.operator_noc = s.operator_ref
+        WHERE
+          t3.timetable_id = t.timetable_id
+          AND t3.date_of_journey = partition_date
+          AND s.date_of_journey = partition_date
+      );
 
     RAISE NOTICE '% Created stops without service table',
         clock_timestamp();
@@ -150,78 +124,65 @@ BEGIN
     RAISE NOTICE '% Creating stops with invalid gps in zone table',
         clock_timestamp();
 
-    EXECUTE format('
-                    CREATE TABLE public.%I AS (
-                      SELECT
-                        DISTINCT timetable_id
-                      FROM
-                        public."Timetable" t
-                        JOIN public."SiriVMPositions" s
-                          ON t.operator_noc = s.operator_ref
-                         AND t.line_name = s.line_name
-                         AND t.journey_code = s.journey_ref
-                      WHERE
-                        t.date_of_journey = %L
-                        AND t.expected_departure_time < (now() - interval ''120'' MINUTE)
-                        AND (s.date_of_journey = %L OR s.date_of_journey = (%L + interval ''1'' DAY))
-                        AND t.actual_departure_time IS NULL
-                        AND (
-                          2 * ASIN(
-                            SQRT(
-                              POWER(
-                                SIN(
-                                  (RADIANS(t.stop_latitude) - RADIANS(s.latitude)) / 2
-                                ),
-                                2
-                              ) + COS(RADIANS(s.latitude)) * COS(RADIANS(t.stop_latitude)) * POWER(
-                                SIN(
-                                  (RADIANS(s.longitude) - RADIANS(t.stop_longitude)) / 2
-                                ),
-                                2
-                              )
-                            )
-                          ) * 6371
-                        ) * 1000 <= 70
-                    );',
-                   concat('stops_w_invalid_gps_in_zone', longdatestring),
-                   partition_date,
-                   partition_date,
-                   partition_date
-            );
+    CREATE TEMP TABLE incomplete_data_tmp_stops_w_invalid_gps_in_zone AS (
+      SELECT
+        DISTINCT timetable_id
+      FROM
+        public."Timetable" t
+        JOIN public."SiriVMPositions" s
+          ON t.operator_noc = s.operator_ref
+         AND t.line_name = s.line_name
+         AND t.journey_code = s.journey_ref
+      WHERE
+        t.date_of_journey = partition_date
+        AND t.expected_departure_time < (now() - recent_stop_interval)
+        AND (s.date_of_journey = partition_date OR s.date_of_journey = next_date)
+        AND t.actual_departure_time IS NULL
+        AND (
+          2 * ASIN(
+            SQRT(
+              POWER(
+                SIN(
+                  (RADIANS(t.stop_latitude) - RADIANS(s.latitude)) / 2
+                ),
+                2
+              ) + COS(RADIANS(s.latitude)) * COS(RADIANS(t.stop_latitude)) * POWER(
+                SIN(
+                  (RADIANS(s.longitude) - RADIANS(t.stop_longitude)) / 2
+                ),
+                2
+              )
+            )
+          ) * 6371
+        ) * 1000 <= 70
+    );
 
     RAISE NOTICE '% Created stops with invalid gps in zone table', clock_timestamp();
 
     RAISE NOTICE '% Creating stops without gps in zone table', clock_timestamp();
 
-    EXECUTE format('
-                    CREATE TABLE public.%I AS
-                    SELECT
-                      DISTINCT timetable_id
-                    FROM
-                      public."Timetable" t
-                      JOIN public."SiriVMPositions" s
-                        ON t.operator_noc = s.operator_ref
-                       AND t.line_name = s.line_name
-                       AND t.journey_code = s.journey_ref
-                    WHERE
-                      t.date_of_journey = %L
-                      AND t.expected_departure_time < (now() - interval ''120'' MINUTE)
-                      AND (s.date_of_journey = %L OR s.date_of_journey = (%L + interval ''1'' DAY))
-                      AND t.actual_departure_time IS NULL
-                      AND NOT EXISTS (
-                        SELECT
-                          1
-                        FROM
-                          public.%I t2
-                        WHERE
-                          t2.timetable_id = t.timetable_id
-                      );',
-                   concat('stops_wo_gps_in_zone_', longdatestring),
-                   partition_date,
-                   partition_date,
-                   partition_date,
-                   concat('stops_w_invalid_gps_in_zone', longdatestring)
-            );
+    CREATE TEMP TABLE incomplete_data_tmp_stops_wo_gps_in_zone AS
+    SELECT
+      DISTINCT timetable_id
+    FROM
+      public."Timetable" t
+      JOIN public."SiriVMPositions" s
+        ON t.operator_noc = s.operator_ref
+       AND t.line_name = s.line_name
+       AND t.journey_code = s.journey_ref
+    WHERE
+      t.date_of_journey = partition_date
+      AND t.expected_departure_time < (now() - recent_stop_interval)
+      AND (s.date_of_journey = partition_date OR s.date_of_journey = next_date)
+      AND t.actual_departure_time IS NULL
+      AND NOT EXISTS (
+        SELECT
+          1
+        FROM
+          incomplete_data_tmp_stops_w_invalid_gps_in_zone t2
+        WHERE
+          t2.timetable_id = t.timetable_id
+      );
 
     RAISE NOTICE '% Created stops without gps in zone table',
         clock_timestamp();
@@ -229,106 +190,72 @@ BEGIN
     RAISE NOTICE '% Populating incomplete reason column in timetable',
         clock_timestamp();
 
-    EXECUTE format('
-                    UPDATE
-                      public."Timetable"
-                    SET
-                      incomplete_reason = 1
-                    WHERE
-                      timetable_id IN (
-                        SELECT
-                          timetable_id
-                        FROM
-                          %I
-                      )
-                      AND date_of_journey = %L;',
-                   concat('stops_wo_nocs_', longdatestring),
-                   partition_date
-            );
+    UPDATE
+      public."Timetable"
+    SET
+      incomplete_reason = 1
+    WHERE
+      timetable_id IN (
+        SELECT
+          timetable_id
+        FROM
+          incomplete_data_tmp_stops_wo_nocs
+      )
+      AND date_of_journey = partition_date;
 
-    EXECUTE format('
-                    UPDATE
-                      public."Timetable"
-                    SET
-                      incomplete_reason = 2
-                    WHERE
-                      timetable_id IN (
-                        SELECT
-                          timetable_id
-                        FROM
-                          %I
-                      )
-                      AND date_of_journey = %L;',
-                   concat('stops_wo_service_', longdatestring),
-                   partition_date
-            );
+    UPDATE
+      public."Timetable"
+    SET
+      incomplete_reason = 2
+    WHERE
+      timetable_id IN (
+        SELECT
+          timetable_id
+        FROM
+          incomplete_data_tmp_stops_wo_service
+      )
+      AND date_of_journey = partition_date;
 
-    EXECUTE format('
-                    UPDATE
-                      public."Timetable"
-                    SET
-                      incomplete_reason = 3
-                    WHERE
-                      timetable_id IN (
-                        SELECT
-                          timetable_id
-                        FROM
-                          %I
-                      )
-                      AND date_of_journey = %L;',
-                   concat('stops_wo_journey_code_', longdatestring),
-                   partition_date
-            );
+    UPDATE
+      public."Timetable"
+    SET
+      incomplete_reason = 3
+    WHERE
+      timetable_id IN (
+        SELECT
+          timetable_id
+        FROM
+          incomplete_data_tmp_stops_wo_journey_code
+      )
+      AND date_of_journey = partition_date;
 
-    EXECUTE format('
-                    UPDATE
-                      public."Timetable"
-                    SET
-                      incomplete_reason = 4
-                    WHERE
-                      timetable_id IN (
-                        SELECT
-                          timetable_id
-                        FROM
-                          %I
-                      )
-                      AND date_of_journey = %L;',
-                   concat('stops_wo_gps_in_zone_', longdatestring),
-                   partition_date
-            );
+    UPDATE
+      public."Timetable"
+    SET
+      incomplete_reason = 4
+    WHERE
+      timetable_id IN (
+        SELECT
+          timetable_id
+        FROM
+          incomplete_data_tmp_stops_wo_gps_in_zone
+      )
+      AND date_of_journey = partition_date;
 
-    EXECUTE format('
-                    UPDATE
-                      public."Timetable"
-                    SET
-                      incomplete_reason = 5
-                    WHERE
-                      timetable_id IN (
-                        SELECT
-                          timetable_id
-                        FROM
-                          %I
-                      )
-                      AND date_of_journey = %L;',
-                   concat('stops_w_invalid_gps_in_zone', longdatestring),
-                   partition_date
-            );
-
-    RAISE NOTICE '% Dropping temp tables',
-        clock_timestamp();
-
-    EXECUTE format('drop table if exists public.%I', concat('stops_wo_nocs_', longdatestring));
-
-    EXECUTE format('drop table if exists public.%I', concat('stops_wo_service_', longdatestring));
-
-    EXECUTE format('drop table if exists public.%I', concat('stops_wo_journey_code_', longdatestring));
-
-    EXECUTE format('drop table if exists public.%I', concat('stops_wo_gps_in_zone_', longdatestring));
-
-    EXECUTE format('drop table if exists public.%I', concat('stops_w_invalid_gps_in_zone', longdatestring));
+    UPDATE
+      public."Timetable"
+    SET
+      incomplete_reason = 5
+    WHERE
+      timetable_id IN (
+        SELECT
+          timetable_id
+        FROM
+          incomplete_data_tmp_stops_w_invalid_gps_in_zone
+      )
+      AND date_of_journey = partition_date;
 
     RAISE NOTICE '% incomplete_data_load complete', clock_timestamp();
-
 END;
 $$;
 
