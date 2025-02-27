d20f3691b246c8a5b23215a068efcf8d6170507a
Gareth Somerville <gareth.somerville@doza.consulting>
2025-02-27 15:40:54 +0000

    Instead redo all stats at the same time for the day before

--- liquibase/sql/125-redo-summaries-evening.sql
+++ liquibase/sql/125-redo-summaries-evening.sql
@@ -1,4 +1,36 @@
-select cron.schedule('Second run adding headway and incomplete reasonings to timetable to capture departure day shift', '0 17 * * *',  $$call public.populate_headway(now()::date - 1);call public.incomplete_data_load(now()::date - 1); $$);
+SELECT cron.schedule(
+               'Update headway & SiriVMposition id',
+               '00 01 * * *',
+               $$CALL public.populate_headway(CURRENT_DATE - '2 day'::interval);CALL public.populate_headway(CURRENT_DATE - '1 day'::interval);CALL public.incomplete_data_load(CURRENT_DATE - '2 day'::interval);CALL public.incomplete_data_load(CURRENT_DATE - '1 day'::interval);$$
+       );
+
+SELECT cron.schedule(
+               'summary_by_stop',
+               '0 2 * * *',
+               $$CALL public.summary_by_stops(CURRENT_DATE - '2 day'::interval);CALL public.summary_by_stops(CURRENT_DATE - '1 day'::interval);$$
+       );
+
+SELECT cron.schedule(
+               'summary_by_services',
+               '20 2 * * *',
+               $$CALL public.summary_by_services(CURRENT_DATE - '2 day'::interval);CALL public.summary_by_services(CURRENT_DATE - '1 day'::interval);$$
+       );
+
+SELECT cron.schedule(
+               'summary_by_operators',
+               '30 2 * * *',
+               $$CALL public.summary_by_operators(CURRENT_DATE - '2 day'::interval);CALL public.summary_by_operators(CURRENT_DATE - '1 day'::interval);$$
+       );
 
 
-select cron.schedule('Second run create_timetable_threshold_summary to capture departure day shift', '30 17 * * *',  $$call create_timetable_threshold_summary(now()::date - 1); $$);
+SELECT cron.schedule(
+               'frequent_summary_services',
+               '0 2 * * *',
+               $$CALL public.frequent_summary_services();$$
+       );
+
+SELECT cron.schedule(
+               'Refresh create_timetable_threshold_summary',
+               '30 02 * * *',
+               $$CALL public.create_timetable_threshold_summary(CURRENT_DATE - '2 day'::interval);CALL public.create_timetable_threshold_summary(CURRENT_DATE - '1 day'::interval);$$
+       );
