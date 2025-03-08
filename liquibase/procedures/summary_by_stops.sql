3314e299949fd489c5281c164bb59d325ad4bf1e
jramsbottom <122462160+jramsbottom@users.noreply.github.com>
2025-03-08 10:13:28 +0000

    Update summary_by_stops.sql

--- liquibase/procedures/summary_by_stops.sql
+++ liquibase/procedures/summary_by_stops.sql
@@ -145,7 +145,7 @@ BEGIN
 					WHERE
 						ttb.date_of_journey = %L
 						and ttb.previous_group_id is null
-						and (t.registered is null or t.registered = true)
+						and (ttb.registered is null or ttb.registered = true)
 				) AS sub
 				WHERE
 					date_of_journey = %L
