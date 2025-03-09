90ce592c5a0893bca073f03cff9c95937b21ce6e
jramsbottom <122462160+jramsbottom@users.noreply.github.com>
2025-03-09 15:07:44 +0000

    Update frequent_summary_services.sql

--- liquibase/procedures/frequent_summary_services.sql
+++ liquibase/procedures/frequent_summary_services.sql
@@ -124,8 +124,7 @@ BEGIN
                                 AND ttb.line_name = es.line_name
                                 AND ttb.service_code = split_part(es.noc_and_line_and_servicecode, '-', -1)
                             WHERE ttb.date_of_journey = partition_date
-                                AND ttb.previous_group_id IS NOT NULL
-                                AND (ttb.registered is null or ttb.registered = true)) AS sub
+                                AND ttb.previous_group_id IS NOT NULL) AS sub
                         WHERE date_of_journey = partition_date
                         GROUP BY operator_noc,
                                 service_code,
