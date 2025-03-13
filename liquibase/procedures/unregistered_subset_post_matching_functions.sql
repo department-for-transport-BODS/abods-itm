9ded0453e221d6eb0a2496914cd73bc3284918c9
jramsbottom <122462160+jramsbottom@users.noreply.github.com>
2025-03-13 12:23:34 +0000

    Create unregistered_subset_post_matching_functions.sql

---
+++ liquibase/procedures/unregistered_subset_post_matching_functions.sql
@@ -0,0 +1,27 @@
+CREATE OR REPLACE PROCEDURE public.unregistered_subset_post_matching_functions(IN partition_date date)
+ LANGUAGE plpgsql
+AS $$
+DECLARE
+    datestring TEXT := to_char(partition_date, 'YYYY-MM-DD');
+BEGIN
+    RAISE NOTICE '% Running unregistered_subset functions %', clock_timestamp(), partition_date::TEXT;
+   
+   	call generate_expected_tables_unregistered_subset(partition_date::date);
+   
+    call populate_headway_unregistered_subset(partition_date::date);
+   
+    call incomplete_data_load_unregistered_subset(partition_date::date);
+   
+    call summary_by_stops_unregistered_subset(partition_date::date);
+   
+    call summary_by_services_unregistered_subset(partition_date::date);
+
+   	call summary_by_operators(partition_date::date);
+   
+    call frequent_summary_services_unregistered_subset(partition_date::date);
+   
+
+    RAISE NOTICE '% Completed unregistered_subset functions for date %', clock_timestamp(), partition_date::TEXT;
+END;
+$$
+;
