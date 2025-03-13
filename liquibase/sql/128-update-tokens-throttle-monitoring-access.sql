0ee0a6f987a0061df5fa612ee2b14a37e5d69046
Karthik Alva <karthik.alva@kpmg.co.uk>
2025-03-13 12:34:38 +0000

    removed null

--- liquibase/sql/128-update-tokens-throttle-monitoring-access.sql
+++ liquibase/sql/128-update-tokens-throttle-monitoring-access.sql
@@ -1,3 +1,3 @@
 ALTER TABLE public."Tokens" 
-ADD COLUMN data_monitoring_access_count INT NULL,
-ADD COLUMN data_monitoring_last_accessed TIMESTAMP NULL;
+ADD COLUMN data_monitoring_access_count INT,
+ADD COLUMN data_monitoring_last_accessed TIMESTAMP;
