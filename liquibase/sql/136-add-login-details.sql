ab8829567ca91bd2658fb550151c8244edd0670d
Gareth Somerville <gareth.somerville@doza.consulting>
2025-03-17 19:01:02 +0000

    Capitalise

--- liquibase/sql/136-add-login-details.sql
+++ liquibase/sql/136-add-login-details.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS public.login_details
 (
-    user_id                       INT       NOT NULL primary key,
+    user_id                       INT       NOT NULL PRIMARY KEY,
     last_login                    TIMESTAMP NOT NULL,
     data_monitoring_access_count  INT,
     data_monitoring_last_accessed TIMESTAMP
@@ -10,6 +10,6 @@ ALTER TABLE public.login_details
     OWNER TO abods_proxy_rw;
 
 INSERT INTO public.login_details (user_id, last_login)
-SELECT user_id, (expires - interval '14' day)
+SELECT user_id, (expires - INTERVAL '14' DAY)
 FROM public."Tokens"
-WHERE user_id NOT IN (SELECT user_id from public.login_details);
+WHERE user_id NOT IN (SELECT user_id FROM public.login_details);
