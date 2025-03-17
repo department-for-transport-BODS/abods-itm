54fff21db7ca8f7d2cfdacd4571c03d3b2c18ae9
Gareth Somerville <gareth.somerville@doza.consulting>
2025-03-17 19:00:33 +0000

    Make it idempotent

--- liquibase/sql/136-add-login-details.sql
+++ liquibase/sql/136-add-login-details.sql
@@ -1,6 +1,6 @@
-CREATE TABLE public.login_details
+CREATE TABLE IF NOT EXISTS public.login_details
 (
-    user_id                       INT NOT NULL primary key,
+    user_id                       INT       NOT NULL primary key,
     last_login                    TIMESTAMP NOT NULL,
     data_monitoring_access_count  INT,
     data_monitoring_last_accessed TIMESTAMP
@@ -11,4 +11,5 @@ ALTER TABLE public.login_details
 
 INSERT INTO public.login_details (user_id, last_login)
 SELECT user_id, (expires - interval '14' day)
-FROM public."Tokens";
+FROM public."Tokens"
+WHERE user_id NOT IN (SELECT user_id from public.login_details);
