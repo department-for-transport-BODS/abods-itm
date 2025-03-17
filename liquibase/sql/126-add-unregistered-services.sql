2ddeea4f76f3c3864917dc775adb7e2e34e87339
Karthik Alva <karthik.alva@kpmg.co.uk>
2025-03-17 13:27:32 +0000

    updated table design as per review comment

--- liquibase/sql/126-add-unregistered-services.sql
+++ liquibase/sql/126-add-unregistered-services.sql
@@ -1,6 +1,13 @@
 ALTER TABLE public."Timetable"
     ADD COLUMN IF NOT EXISTS registered BOOLEAN;
 
-ALTER TABLE public."Tokens" 
-ADD COLUMN data_monitoring_access_count INT,
-ADD COLUMN data_monitoring_last_accessed TIMESTAMP;
+
+CREATE TABLE public.login_details (
+    user_id INT NOT NULL,
+    data_monitoring_access_count INT,
+    data_monitoring_last_accessed TIMESTAMP
+);
+
+CREATE INDEX if not exists login_details_idx ON public.login_details USING btree (user_id);
+
+ALTER TABLE public.login_details OWNER TO abods_proxy_rw;
