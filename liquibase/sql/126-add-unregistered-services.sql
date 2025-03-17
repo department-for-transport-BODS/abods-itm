d9092163783e68c2779e3df36f263bbb240ebea6
Karthik Alva <karthik.alva@kpmg.co.uk>
2025-03-17 14:24:51 +0000

    changes as per review comments

--- liquibase/sql/126-add-unregistered-services.sql
+++ liquibase/sql/126-add-unregistered-services.sql
@@ -8,6 +8,4 @@ CREATE TABLE public.login_details (
     data_monitoring_last_accessed TIMESTAMP
 );
 
-CREATE INDEX if not exists login_details_idx ON public.login_details USING btree (user_id);
-
 ALTER TABLE public.login_details OWNER TO abods_proxy_rw;
