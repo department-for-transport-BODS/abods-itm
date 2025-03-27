883eae32fc951531230b5ec4dc663c5ce53406f8
Karthik Alva <karthik.alva@kpmg.co.uk>
2025-03-27 17:02:39 +0000

    changes to updated excess wait time nullable headway column and populate headway fix

--- liquibase/sql/139-update-excess-wait-time-column.sql
+++ liquibase/sql/139-update-excess-wait-time-column.sql
@@ -2,3 +2,9 @@
 -- as opposed to 0 where excess wait time is 0 for journeys
 ALTER TABLE public.timetable_frequent_summary_services
 ALTER COLUMN excess_wait_Time DROP NOT NULL;
+
+ALTER TABLE public.timetable_frequent_summary_services
+ALTER COLUMN expected_headway DROP NOT NULL;
+
+ALTER TABLE public.timetable_frequent_summary_services
+ALTER COLUMN actual_headway DROP NOT NULL;
