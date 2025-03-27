c1282a84f557861b1f2795252cfefe871b50a823
Karthik Alva <karthik.alva@kpmg.co.uk>
2025-03-27 16:39:25 +0000

    changes to updated excess wait time nullable headway column and populate headway fix

--- liquibase/procedures/populate_headway.sql
+++ liquibase/procedures/populate_headway.sql
@@ -11,56 +11,37 @@ BEGIN
     DROP TABLE IF EXISTS temp_timetable_headway;
 
     CREATE TABLE temp_timetable_headway AS
-    SELECT
-      *,
-      extract(
-        epoch
-        FROM
-          x.actual_departure_time - x.previous_actual_departure_time
-      ) AS actual_headway
-    FROM
-      (
-        SELECT
-          timetable_id,
-          COALESCE(actual_departure_time, timestamp_after_estimate) AS actual_departure_time,
-          lag(
-            COALESCE(actual_departure_time, timestamp_after_estimate)
-          ) OVER (
-            PARTITION BY operator_noc,
-            line_name,
-            date_of_journey,
-            stop_id,
-            stop_index
-            ORDER BY
-              stop_id,
-              stop_index ASC,
-              expected_departure_time ASC
-          ) AS previous_actual_departure_time
-        FROM
-          public."Timetable"
-        WHERE date_of_journey = pt_date
-          AND expected_departure_time < earliest_departure_time
-          AND previous_group_id IS NOT NULL
-          AND (
-            actual_departure_time IS NOT NULL
-            OR timestamp_after_estimate IS NOT NULL
-          )
-      ) x
-    WHERE
-      x.previous_actual_departure_time IS NOT NULL;
+    SELECT 
+      t1.timetable_id,
+      COALESCE(EXTRACT(EPOCH FROM (
+          COALESCE(t2.actual_departure_time, t2.timestamp_after_estimate) 
+          - COALESCE(t1.actual_departure_time, t1.timestamp_after_estimate)
+          )),0) as actual_headway,
+      COALESCE(EXTRACT(EPOCH FROM (
+          t2.expected_departure_time 
+          - t1.expected_departure_time
+          )),0) as expected_headway
+      from public."Timetable" t1 left join public."Timetable" t2 
+      on t1.previous_group_id = t2.group_id 
+      and t1.stop_id = t2.stop_id and t1.stop_index = t2.stop_index 
+      and t1.direction = t2.direction
+      where t1.date_of_journey = pt_date
+      and t2.date_of_journey = pt_date
+      and t1.previous_group_id IS NOT NULL
+      AND t1.expected_departure_time < earliest_departure_time;
 
     RAISE NOTICE '% Updating timetable table with headway data', clock_timestamp();
 
     UPDATE
       public."Timetable" y
     SET
-      headway_time_difference = y.expected_headway - x.actual_headway,
+      headway_time_difference = x.expected_headway - x.actual_headway,
       actual_headway = x.actual_headway
+      expected_headway = x.expected_headway
     FROM
       temp_timetable_headway x
     WHERE x.timetable_id = y.timetable_id
-      AND y.date_of_journey = pt_date
-      AND y.expected_departure_time < earliest_departure_time;
+      AND y.date_of_journey = pt_date;
 
     RAISE NOTICE '% Dropping temp tables', clock_timestamp();
 
