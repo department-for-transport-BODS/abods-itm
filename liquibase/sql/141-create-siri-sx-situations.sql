d03143d62f25a1c621ddb9ec21842c8718140a99
Andy Crow <andy.crow@doza.consulting>
2025-04-24 12:38:55 -0300

    Add migration for siri_sx_situations table

---
+++ liquibase/sql/141-create-siri-sx-situations.sql
@@ -0,0 +1,20 @@
+CREATE TABLE public.siri_sx_situations (
+    id SERIAL PRIMARY KEY,
+    producer_ref TEXT NOT NULL,
+    situation_number TEXT NOT NULL,
+    version TEXT,
+    operator_noc TEXT,
+    line_name TEXT,
+    direction TEXT,
+    date_of_journey TIMESTAMPTZ,
+    start_date TIMESTAMPTZ,
+    end_date TIMESTAMPTZ,
+    journey_code TEXT,
+    condition TEXT,
+    progress TEXT,
+    event_timestamp TIMESTAMPTZ,
+    creation_time TIMESTAMPTZ,
+    UNIQUE (producer_ref, situation_number, version)
+);
+
+ALTER TABLE public.siri_sx_situations OWNER TO abods_rw;
