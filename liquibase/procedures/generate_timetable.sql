aaf9209607a9cd63e0f545d6105b967b26f5169c
jramsbottom <122462160+jramsbottom@users.noreply.github.com>
2025-05-02 16:01:12 +0100

    Update generate_timetable.sql

--- liquibase/procedures/generate_timetable.sql
+++ liquibase/procedures/generate_timetable.sql
@@ -1,7 +1,6 @@
-create or replace procedure generate_timetable(IN partition_date date)
-    language plpgsql
-as
-$$
+CREATE OR REPLACE PROCEDURE public.generate_timetable(IN partition_date date)
+ LANGUAGE plpgsql
+AS $procedure$
 
 declare
     longdatestring   text := to_char(partition_date, 'YYYY_MM_DD');
@@ -807,11 +806,11 @@ begin
               journey_code,
               date_of_journey AS date_of_journey,
               departure_time,
-              b.id AS stop_id,
+              coalesce(b.id::text, '''') AS stop_id,
               ST_Y(b.location)::real lt,
               ST_X(b.location)::real AS lon,
               common_name AS stopname,
-              b.stop_index::text,
+              a.stop_index,
               b.common_name AS stop_name,
               a.is_timing_point,
               b.locality_id,
@@ -820,7 +819,7 @@ begin
               day_of_week,
               stop_type,
               group_id,
-              b.atco_code::text,
+              a.atco_code,
               row_number() over(
                 PARTITION BY operator_ref,
                 line_name,
@@ -1253,6 +1252,6 @@ begin
 
     RAISE NOTICE '% generate_timetable complete', clock_timestamp();
 end;
-$$;
-
-alter procedure generate_timetable owner to abods_proxy_rw;
+$procedure$
+;
+;
