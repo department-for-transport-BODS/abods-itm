6354df152be8374479a9475c917babd68a22f623
jramsbottom <122462160+jramsbottom@users.noreply.github.com>
2025-06-09 10:18:09 +0100

    Update 148-add-reprocessing-columns.sql

--- liquibase/sql/148-add-reprocessing-columns.sql
+++ liquibase/sql/148-add-reprocessing-columns.sql
@@ -1,3 +1,7 @@
+alter foreign table if exists bods.organisation_datasetrevision
+ADD COLUMN modified_before_reprocessing TIMESTAMPTZ,
+ADD COLUMN status_before_reprocessing TEXT;
+
 ALTER TABLE organisation_datasetrevision
 ADD COLUMN modified_before_reprocessing TIMESTAMPTZ,
 ADD COLUMN status_before_reprocessing TEXT;
