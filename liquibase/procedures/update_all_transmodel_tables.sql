632ea154219f49638235fa1d20c21a3f2cae0a0d
jramsbottom <122462160+jramsbottom@users.noreply.github.com>
2025-06-18 15:56:03 +0100

    Update update_all_transmodel_tables.sql

--- liquibase/procedures/update_all_transmodel_tables.sql
+++ liquibase/procedures/update_all_transmodel_tables.sql
@@ -33,6 +33,8 @@ begin
     call public.update_transmodel_servicepattern_admin_areas();
     raise notice 'Running update_transmodel_tracks at %', clock_timestamp();
     call public.update_transmodel_tracks();
+    raise notice 'Running update_transmodel_servicepatterndistance at %', clock_timestamp();
+    call update_transmodel_servicepatterndistance()    
 end;
 $$;
 
