eb117b5fd252a75bfce624552ec1184ea69fca9a
Karthik Alva <karthik.alva@kpmg.co.uk>
2025-06-19 12:18:47 +0100

    added new bus starting ealry test case

--- ingestion_pipelines/sirivm_otp_matching_function/sirivm_otp_matching_function/matcher/historic_examples/scem_9_27_2024_11_06/test_matching.py
+++
@@ -1,9 +0,0 @@
-"""Bus starting early"""
-
-from ..util import run_historic_matching_test
-
-matches = []
-
-
-def test_historic_match() -> None:
-    assert run_historic_matching_test(__file__) == matches
