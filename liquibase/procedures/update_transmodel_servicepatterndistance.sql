4b47f7d7eb9706d899d9d340e795e8bcbe053ebb
jramsbottom <122462160+jramsbottom@users.noreply.github.com>
2025-06-23 15:08:22 +0100

    Update update_transmodel_servicepatterndistance.sql

--- liquibase/procedures/update_transmodel_servicepatterndistance.sql
+++ liquibase/procedures/update_transmodel_servicepatterndistance.sql
@@ -8,10 +8,11 @@ begin
     insert into public.transmodel_servicepatterndistance (distance,
                                                   geom,
                                                   service_pattern_id)
-    select distance,
-           geom,
-           service_pattern_id
+    select ts.distance,
+           ts.geom,
+           ts.service_pattern_id
     from bods.transmodel_servicepatterndistance ts
+    right join public.transmodel_servicepattern tsp on tsp.id = ts.service_pattern_id
     where ts.service_pattern_id > max_current
     on conflict do nothing;
 end;
