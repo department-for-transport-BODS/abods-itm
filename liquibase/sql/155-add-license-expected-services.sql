660de19b50b7dae1991d3d5583105500c1a8c441
jramsbottom <122462160+jramsbottom@users.noreply.github.com>
2025-07-10 15:19:47 +0100

    Update 155-add-license-expected-services.sql

--- liquibase/sql/155-add-license-expected-services.sql
+++ liquibase/sql/155-add-license-expected-services.sql
@@ -1,1 +1,23 @@
 ALTER TABLE service_details ADD COLUMN IF NOT EXISTS license TEXT;
+
+DROP MATERIALIZED VIEW IF EXISTS public.expected_services;
+
+CREATE MATERIALIZED VIEW public.expected_services
+TABLESPACE pg_default
+AS
+SELECT DISTINCT 
+    esbd.date_of_journey,
+    sd.noc_and_line_and_servicecode,
+    sd.operator_noc,
+    esbd.license,
+    sd.line_name,
+    sd.service_name,
+    esbd.admin_area_id,
+    esbd.total_distance,
+    esbd.avl_true_distance
+FROM 
+    expected_services_by_date esbd
+LEFT JOIN 
+    service_details sd 
+    ON esbd.noc_and_line_and_servicecode = sd.noc_and_line_and_servicecode
+WITH DATA;
