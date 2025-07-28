da3152c817e8a5a94538a358161ba8089ed483a0
jramsbottom <122462160+jramsbottom@users.noreply.github.com>
2025-07-28 11:22:18 +0100

    Update 155-add-license-expected-services.sql

--- liquibase/sql/155-add-license-expected-services.sql
+++ liquibase/sql/155-add-license-expected-services.sql
@@ -6,7 +6,7 @@ ALTER TABLE service_details
 ADD COLUMN IF NOT EXISTS admin_areas int[];
 
 ALTER TABLE service_details 
-ADD COLUMN IF NOT EXISTS license TEXT;
+ADD COLUMN IF NOT EXISTS license VARCHAR(255);
 
 CREATE MATERIALIZED VIEW public.expected_services
 TABLESPACE pg_default
