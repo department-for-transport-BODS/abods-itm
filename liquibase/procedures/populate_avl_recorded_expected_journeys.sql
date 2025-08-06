af291c2851c73c54063bc7857ccffafe25ed88ad
jramsbottom <122462160+jramsbottom@users.noreply.github.com>
2025-08-06 15:50:19 +0100

    Update populate_avl_recorded_expected_journeys.sql

--- liquibase/procedures/populate_avl_recorded_expected_journeys.sql
+++ liquibase/procedures/populate_avl_recorded_expected_journeys.sql
@@ -1,30 +1,24 @@
-CREATE OR REPLACE PROCEDURE public.populate_avl_recorded_expected_journeys(IN partition_date date DEFAULT (CURRENT_DATE - '1 day'::interval))
- LANGUAGE plpgsql
+CREATE OR REPLACE PROCEDURE public.populate_avl_recorded_expected_journeys(
+    IN partition_date date DEFAULT (CURRENT_DATE - '1 day'::interval)
+)
+LANGUAGE plpgsql
 AS $procedure$
 BEGIN
     RAISE NOTICE '% Start running populate_avl_recorded_expected_journeys ', clock_timestamp();
-   
-   	UPDATE public.expected_journeys
-	SET avl_recorded = null
-	WHERE date_of_journey = partition_date;
-   
-	UPDATE public.expected_journeys ej
-	SET avl_recorded = FALSE
-	WHERE EXISTS (
-	    SELECT 1
-	    FROM public."Timetable" t
-	    WHERE t.group_id = ej.group_id
-	      AND t.date_of_journey = partition_date
-	      AND t.incomplete_reason IN (1, 2, 3)
-	)
-	AND ej.date_of_journey = partition_date;
-	
-	UPDATE public.expected_journeys
-	SET avl_recorded = TRUE
-	WHERE avl_recorded IS NULL
-	AND date_of_journey = partition_date;
+
+    UPDATE public.expected_journeys ej
+    SET avl_recorded = CASE
+        WHEN EXISTS (
+            SELECT 1
+            FROM public."Timetable" t
+            WHERE t.group_id = ej.group_id
+              AND t.date_of_journey = partition_date
+              AND t.incomplete_reason IN (1, 2, 3)
+        ) THEN FALSE
+        ELSE TRUE
+    END
+    WHERE ej.date_of_journey = partition_date;
 
     RAISE NOTICE '% populate_avl_recorded_expected_journeys complete', clock_timestamp();
 END;
-$procedure$
-;
+$procedure$;
