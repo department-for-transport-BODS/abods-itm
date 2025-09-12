d55c2504456f541b6ef91561e20ec677f95ad08b
jramsbottom <122462160+jramsbottom@users.noreply.github.com>
2025-09-12 16:14:15 +0100

    Update update_expected_service_distances.sql (#653)
    * Update update_expected_service_distances.sql
    
    * Update liquibase/procedures/update_expected_service_distances.sql
    
    Co-authored-by: alvak1 <95031756+alvak1@users.noreply.github.com>
    
    * Update liquibase/procedures/update_expected_service_distances.sql
    
    Co-authored-by: alvak1 <95031756+alvak1@users.noreply.github.com>
    
    * Update update_expected_service_distances.sql
    
    ---------
    
    Co-authored-by: alvak1 <95031756+alvak1@users.noreply.github.com>

--- liquibase/procedures/update_expected_service_distances.sql
+++ liquibase/procedures/update_expected_service_distances.sql
@@ -1,24 +1,29 @@
-CREATE OR REPLACE PROCEDURE public.update_expected_service_distances(IN partition_date date DEFAULT (CURRENT_DATE - '1 day'::interval))
- LANGUAGE plpgsql
+CREATE OR REPLACE PROCEDURE public.update_expected_service_distances(
+    IN partition_date date DEFAULT (CURRENT_DATE - '1 day'::interval)
+)
+LANGUAGE plpgsql
 AS $procedure$
 BEGIN
-    WITH service_summary AS (
+    WITH journeys AS (
+        SELECT DISTINCT vehiclejourney_id, servicepattern_id
+        FROM public."Timetable"
+        WHERE date_of_journey = partition_date
+    ),
+    service_summary AS (
         SELECT 
-            vj.service_pattern_id,
+            dt.servicepattern_id AS service_pattern_id,
             ej.noc_and_line_and_servicecode,
             ej.date_of_journey,
             COUNT(*) AS total_count,
-            COUNT(*) FILTER (WHERE avl_recorded = TRUE) AS avl_true_count,
-            COUNT(*) FILTER (WHERE avl_recorded IS DISTINCT FROM TRUE) AS avl_false_count
+            COUNT(*) FILTER (WHERE ej.avl_recorded = TRUE) AS avl_true_count,
+            COUNT(*) FILTER (WHERE ej.avl_recorded IS DISTINCT FROM TRUE) AS avl_false_count
         FROM 
             public.expected_journeys ej
         JOIN 
-            transmodel_vehiclejourney vj 
-            ON ej.vehicle_journey_id = vj.id
-        WHERE 
-            ej.date_of_journey = partition_date
+            journeys dt
+            ON dt.vehiclejourney_id = ej.vehicle_journey_id
         GROUP BY 
-            vj.service_pattern_id,
+            dt.servicepattern_id,
             ej.noc_and_line_and_servicecode,
             ej.date_of_journey
     ),
@@ -56,9 +61,8 @@ BEGIN
     WHERE 
         esbd.date_of_journey = ad.date_of_journey
         AND esbd.noc_and_line_and_servicecode = ad.noc_and_line_and_servicecode;
-       
-REFRESH MATERIALIZED VIEW expected_services;
 
+    REFRESH MATERIALIZED VIEW expected_services;
 END;
-$procedure$
-;
+$procedure$;
+
