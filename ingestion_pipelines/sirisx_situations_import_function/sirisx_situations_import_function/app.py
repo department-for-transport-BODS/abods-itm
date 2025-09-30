b6dfb2589aa676584c2227bbdc0b9b9af7121edc
jramsbottom <122462160+jramsbottom@users.noreply.github.com>
2025-09-30 14:50:02 +0100

    BODS-8782 Cancellations fixes (#697)
    * Set is_cancelled to FALSE for all expected_journeys on partition date before flagging cancelled journeys; remove extra columns from DISTINCT to prevent uncancelled services being removed
    
    * fixing issues with not removing normal_service from expected journeys
    
    * fix order by error
    
    * Update cancellations URL to prod
    
    * add variable for siri prod endpoint
    
    * github error
    
    * update VERSION
    
    * Update version to v1.0.97
    
    * file formatted
    
    ---------
    
    Co-authored-by: Karthik Alva <karthik.alva@kpmg.co.uk>

--- ingestion_pipelines/sirisx_situations_import_function/sirisx_situations_import_function/app.py
+++ ingestion_pipelines/sirisx_situations_import_function/sirisx_situations_import_function/app.py
@@ -1,5 +1,6 @@
 from datetime import UTC, datetime
 from io import BytesIO
+from os import environ
 
 import requests
 from aws_lambda_powertools import Logger
@@ -9,9 +10,7 @@ from psycopg2.extras import execute_batch
 from .models import SituationRecord
 from .shared.db import setup_db
 
-SIRI_SX_CANCELLATIONS_URL = (
-    "https://6tfu67dcng.execute-api.eu-west-2.amazonaws.com/v1/siri-sx"
-)
+siri_sx_cancellations_url = environ.get("SIRI_SX_CANCELLATIONS_URL")
 NS_URI = "http://www.siri.org.uk/siri"
 
 logger = Logger()
@@ -212,7 +211,7 @@ def insert_rows(rows: list[SituationRecord]) -> None:
 
 def lambda_handler(_event: dict, _context: dict) -> None:
     logger.info("Retrieving XML from SIRI SX")
-    response = requests.get(SIRI_SX_CANCELLATIONS_URL, timeout=30)
+    response = requests.get(siri_sx_cancellations_url, timeout=30)
     response.raise_for_status()
     xml_bytes = response.content
     rows = parse_xml(xml_bytes)
--- liquibase/procedures/flag_cancelled_expected_journeys.sql
+++ liquibase/procedures/flag_cancelled_expected_journeys.sql
@@ -3,34 +3,35 @@ create or replace procedure flag_cancelled_expected_journeys(IN partition_date d
 as
 $$
 begin
+    -- Set all is_cancelled to FALSE for the partition date before flagging cancelled ones
+    UPDATE expected_journeys 
+    SET is_cancelled = FALSE 
+    WHERE date_of_journey = partition_date;
 
     RAISE NOTICE '% Flagging cancelled journeys for %', clock_timestamp(), partition_date::text;
 
     UPDATE expected_journeys ej
     SET is_cancelled = TRUE
     FROM (
-        SELECT DISTINCT ON (producer_ref, situation_number)
+        SELECT DISTINCT ON (producer_ref, operator_noc, line_name, journey_code, direction)
             producer_ref,
-            situation_number,
             operator_noc,
             line_name,
             journey_code,
             direction,
             date_of_journey,
-            condition,
-            progress,
-            event_timestamp
+            condition
         FROM siri_sx_situations
         WHERE date_of_journey = partition_date
-        ORDER BY producer_ref, situation_number, event_timestamp DESC
+        ORDER BY producer_ref, operator_noc, line_name, journey_code, direction, event_timestamp DESC
     ) latest_situation
     WHERE ej.date_of_journey = latest_situation.date_of_journey
     AND ej.operator_noc = latest_situation.operator_noc
     AND ej.line_name = latest_situation.line_name
     AND ej.journey_code = latest_situation.journey_code
     AND ej.direction = latest_situation.direction
-    AND latest_situation.condition != 'normalService' -- # Cancelled service
-    AND latest_situation.progress = 'open'; -- # On-going situation
+    AND latest_situation.condition != 'normalService'; -- # Cancelled service
+    
 
     RAISE NOTICE '% flag_cancelled_expected_journeys complete', clock_timestamp();
 
