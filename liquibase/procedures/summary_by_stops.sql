ea50592e877ca8454702773ba739a165b75176c4
jramsbottom <122462160+jramsbottom@users.noreply.github.com>
2025-11-06 11:10:42 +0000

    1.1.01 to main (#743)
    * Ruff action pinned to ruff 0.13.0
    
    * Bump lxml from 6.0.1 to 6.0.2 in /ingestion_pipelines
    
    Bumps [lxml](https://github.com/lxml/lxml) from 6.0.1 to 6.0.2.
    - [Release notes](https://github.com/lxml/lxml/releases)
    - [Changelog](https://github.com/lxml/lxml/blob/master/CHANGES.txt)
    - [Commits](https://github.com/lxml/lxml/compare/lxml-6.0.1...lxml-6.0.2)
    
    ---
    updated-dependencies:
    - dependency-name: lxml
      dependency-version: 6.0.2
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump version from v1.0.99 to v1.1
    
    * Bump duckdb from 1.1.3 to 1.4.1 in /ingestion_pipelines
    
    Bumps [duckdb](https://github.com/duckdb/duckdb-python) from 1.1.3 to 1.4.1.
    - [Changelog](https://github.com/duckdb/duckdb-python/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/duckdb/duckdb-python/commits/v1.4.1)
    
    ---
    updated-dependencies:
    - dependency-name: duckdb
      dependency-version: 1.4.1
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump aws-lambda-powertools from 3.9.0 to 3.22.0 in /ingestion_pipelines
    
    Bumps [aws-lambda-powertools](https://github.com/aws-powertools/powertools-lambda-python) from 3.9.0 to 3.22.0.
    - [Release notes](https://github.com/aws-powertools/powertools-lambda-python/releases)
    - [Changelog](https://github.com/aws-powertools/powertools-lambda-python/blob/develop/CHANGELOG.md)
    - [Commits](https://github.com/aws-powertools/powertools-lambda-python/compare/v3.9.0...v3.22.0)
    
    ---
    updated-dependencies:
    - dependency-name: aws-lambda-powertools
      dependency-version: 3.22.0
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump boto3 from 1.40.25 to 1.40.59 in /ingestion_pipelines
    
    Bumps [boto3](https://github.com/boto/boto3) from 1.40.25 to 1.40.59.
    - [Release notes](https://github.com/boto/boto3/releases)
    - [Commits](https://github.com/boto/boto3/compare/1.40.25...1.40.59)
    
    ---
    updated-dependencies:
    - dependency-name: boto3
      dependency-version: 1.40.59
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Fix for issue where difference of actual departure between stops is showing up as negative (#731)
    
    * pinning formatter version to avoid unexpected error
    
    * fix for negative actual departure time difference between stops
    
    * fix for negative actual departure time difference between stops
    
    * Fix/changelog idempotency (#727)
    
    * Replace noc-adminarea in prep for airflow (#666)
    
    * Replace noc-adminarea in prep for airflow
    
    * Add permissions to nw noc_adminarea
    
    * drop after understanding view
    
    * adding idempotency
    
    * will only perform majority if materializd viw public.noc_adminarea exists
    
    ---------
    
    Co-authored-by: alvak1 <95031756+alvak1@users.noreply.github.com>
    Co-authored-by: PCridKPMG <135717977+PCridKPMG@users.noreply.github.com>
    Co-authored-by: Pete Cridland <peter.cridland@kpmg.co.uk>
    
    * Run changeset 158 on change to prevent error
    
    * Bump version from v1.1 to v1.1.01
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: Pete Cridland <peter.cridland@kpmg.co.uk>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: alvak1 <95031756+alvak1@users.noreply.github.com>
    Co-authored-by: PCridKPMG <135717977+PCridKPMG@users.noreply.github.com>
    Co-authored-by: Laurence Jones <laurence.jones@doza.consulting>

--- liquibase/procedures/summary_by_stops.sql
+++ liquibase/procedures/summary_by_stops.sql
@@ -95,8 +95,8 @@ BEGIN
 							ELSE 0
 						END AS max_late,
 						(ttb.timestamp_after_estimate is not null) AS estimated,
-						LAG(ttb.expected_departure_time) OVER (PARTITION BY ttb.group_id, ttb.vehiclejourney_id ORDER BY ttb.expected_departure_time) AS previous_stop_expected_departure,
-						LAG(COALESCE(ttb.actual_departure_time, ttb.timestamp_after_estimate)) OVER (PARTITION BY ttb.group_id, ttb.vehiclejourney_id ORDER BY ttb.expected_departure_time) AS previous_stop_actual_departure,
+						LAG(ttb.expected_departure_time) OVER (PARTITION BY ttb.group_id, ttb.vehiclejourney_id ORDER BY ttb.expected_departure_time, ttb.stop_index) AS previous_stop_expected_departure,
+						LAG(COALESCE(ttb.actual_departure_time, ttb.timestamp_after_estimate)) OVER (PARTITION BY ttb.group_id, ttb.vehiclejourney_id ORDER BY ttb.expected_departure_time, ttb.stop_index) AS previous_stop_actual_departure,
 						ttb.incomplete_reason AS incomplete_reason
 					FROM
 					public.%I ttb
