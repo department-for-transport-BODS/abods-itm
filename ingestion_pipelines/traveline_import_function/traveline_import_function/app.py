09c4df3277bfea603e7185e17460e9d5bb2c1d4f
Caelainn Brinkley <caelainn.brinkley@kainos.com>
2026-06-22 13:42:22 +0100

    Adding in blank space at the end

--- ingestion_pipelines/traveline_import_function/traveline_import_function/app.py
+++ ingestion_pipelines/traveline_import_function/traveline_import_function/app.py
@@ -72,3 +72,4 @@ def lambda_handler(_event: dict, _context: dict) -> None:
         raise TravelineImportError(
             "NOC_ROLE_ARN environment variable must be set for cross-account access",
         )
+        
