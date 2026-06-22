4a4de52d0c255d7bcfbd5528f729ab71904907af
Caelainn Brinkley <caelainn.brinkley@kainos.com>
2026-06-22 13:30:16 +0100

    fix: updating app.py file to pass ruff tests

--- ingestion_pipelines/traveline_import_function/traveline_import_function/app.py
+++ ingestion_pipelines/traveline_import_function/traveline_import_function/app.py
@@ -8,7 +8,7 @@ from psycopg2.extras import execute_values
 
 from .shared.db import setup_db
 
-# TRAVELINE_NOC_URL = "https://www.travelinedata.org.uk/wp-content/themes/desktop/qeight_download.php?allGroupsD=-1&allRegionD=-1&allModeD=-1&allCessationD=-1&searchTextD=&maxPage=231&selectPageId=1&downloadType=CSV&submit=Download"
+# TRAVELINE_NOC_URL = "https://www.travelinedata.org.uk/wp-content/themes/desktop/qeight_download.php?allGroupsD=-1&allRegionD=-1&allModeD=-1&allCessationD=-1&searchTextD=&maxPage=231&selectPageId=[...]"
 
 logger = Logger()
 conn = setup_db()
@@ -50,58 +50,11 @@ def resolve_noclines_key(s3_client, bucket: str, prefix: str) -> str:
 
     if not candidates:
         raise Exception(
-            f"No '{target_name}' found under s3://{bucket}/{prefix}"
+            f"No '{target_name}' found under s3://{bucket}/{prefix}",
         )
 
     selected = max(candidates, key=lambda o: o["LastModified"])
     return selected["Key"]
-
-
-# def lambda_handler(_event: dict, _context: dict) -> None:
-#     logger.info("Retrieving data from Traveline")
-
-#     noc_table = petl.fromcsv(TRAVELINE_NOC_URL).distinct("NOCCODE")
-
-#     logger.info("Converting data to tuples")
-
-#     rows = tuple(
-#         (row["NOCCODE"], row["OperatorPublicName"], row["Licence"], row["Mode"])
-#         for row in noc_table.dicts()
-#     )
-
-#     with conn.cursor() as cur:
-#         logger.info("Attempting to create table if not exists")
-
-#         cur.execute(
-#             """
-#             CREATE TABLE IF NOT EXISTS traveline_operators (
-#                 noc_code varchar NOT NULL,
-#                 "name" varchar NULL,
-#                 licence varchar NULL,
-#                 "mode" varchar NULL,
-#                 CONSTRAINT travelinedata_pk PRIMARY KEY (noc_code)
-#             );
-#             """,
-#         )
-
-#         logger.info("Inserting NOC data")
-
-#         execute_values(
-#             cur,
-#             """
-#             INSERT INTO traveline_operators (
-#                 noc_code,
-#                 name,
-#                 licence,
-#                 mode
-#             )
-#             VALUES %s
-#             ON CONFLICT (noc_code)
-#                 DO UPDATE SET name = EXCLUDED.name
-#             """,
-#             rows,
-#             page_size=5000,
-#         )
 
 
 def lambda_handler(_event: dict, _context: dict) -> None:
@@ -117,7 +70,9 @@ def lambda_handler(_event: dict, _context: dict) -> None:
     if not region:
         raise Exception("NOC_BUCKET_REGION environment variable must be set")
     if not role_arn:
-    raise Exception("NOC_ROLE_ARN environment variable must be set for cross-account access")
+        raise Exception(
+            "NOC_ROLE_ARN environment variable must be set for cross-account access",
+        )
 
     logger.info("Retrieving noclines data from S3")
 
