e0e4b6c011005f6a2ad31cb8660de2401edad6fe
Tilly Softly <tilly.softly@arup.com>
2026-09-21 13:57:31 +0100

    update sql to check if view exists

--- liquibase/sql/166-update-foreign-schema-otc_inactiveservice-registration_number.sql
+++ liquibase/sql/166-update-foreign-schema-otc_inactiveservice-registration_number.sql
@@ -2,19 +2,33 @@ DO $$
   declare otc_inactiveservice_def text;
   declare exec_text text;
 BEGIN
-  otc_inactiveservice_def := pg_get_viewdef('public.bods_otcinactiveservice');
-  drop view public.bods_otcinactiveservice;
-  IF EXISTS(SELECT *
-    FROM information_schema.columns
-    WHERE table_name='otc_inactiveservice'
-    AND table_schema='bods'
-    and column_name='registration_number')
+  IF EXISTS(SELECT * from public.bods_otcinactiveservice)
   THEN
-      ALTER FOREIGN table bods.otc_inactiveservice
-      alter column registration_number
-      type varchar(255);
+    otc_inactiveservice_def := pg_get_viewdef('public.bods_otcinactiveservice');
+    drop view public.bods_otcinactiveservice;
+    IF EXISTS(SELECT *
+      FROM information_schema.columns
+      WHERE table_name='otc_inactiveservice'
+      AND table_schema='bods'
+      and column_name='registration_number')
+    THEN
+        ALTER FOREIGN table bods.otc_inactiveservice
+        alter column registration_number
+        type varchar(255);
+    END IF;
+    exec_text := format('create view public.bods_otcinactiveservice as %s', 
+        otc_inactiveservice_def);
+    execute exec_text;
+  ELSE
+    IF EXISTS(SELECT *
+      FROM information_schema.columns
+      WHERE table_name='otc_inactiveservice'
+      AND table_schema='bods'
+      and column_name='registration_number')
+    THEN
+        ALTER FOREIGN table bods.otc_inactiveservice
+        alter column registration_number
+        type varchar(255);
+    END IF;
   END IF;
-  exec_text := format('create view public.bods_otcinactiveservice as %s', 
-      otc_inactiveservice_def);
-  execute exec_text;
 END $$;
